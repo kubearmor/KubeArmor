691e53fcc930126e9c8faf03ed28ffdf687af523
Jaehyun Nam <jaehyun.nam@outlook.com>
2020-12-04 02:46:49 +0000

    Added microk8s

--- .travis/prepare.sh
+++ .travis/prepare.sh
@@ -2,3 +2,10 @@
 
 sudo apt-get update
 sudo apt-get install -y linux-headers-$(uname -r)
+
+sudo snap install microk8s --classic
+
+sudo microk8s kubectl get nodes
+sudo microk8s kubectl get services
+
+sudo microk8s kubectl config view --raw > $HOME/.kube/config
