81b5db588a2da368594a8ebe5676ac80d2a5756a
Jaehyun Nam <jn@accuknox.com>
2020-12-04 11:54:34 +0900

    Update prepare.sh

--- .travis/prepare.sh
+++ .travis/prepare.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
 
 sudo apt-get update
+
+# install kernel-headers
 sudo apt-get install -y linux-headers-$(uname -r)
 
+# install microk8s
 sudo snap install microk8s --classic
 
+# check microk8s
 sudo microk8s kubectl get nodes
 sudo microk8s kubectl get services
 
-sudo microk8s kubectl config view --raw > $HOME/.kube/config
+# copy k8s config
+sudo microk8s kubectl config view --raw | sudo tee $HOME/.kube/config
+
+# download kubectl
+curl -Lo kubectl https://storage.googleapis.com/kubernetes-release/release/v1.18.1/bin/linux/amd64/kubectl && chmod +x kubectl && sudo mv kubectl /usr/local/bin/
