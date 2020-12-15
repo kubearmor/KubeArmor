3d5816a0a503a3132f5cf8f3479e213c81fa8b57
Jaehyun Nam <jaehyun.nam@outlook.com>
2020-12-15 08:19:45 +0000

    Updated Travis-CI

---
+++ .travis/go_test.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+KBA_HOME=`dirname $(realpath "$0")`/..
+
+cd $KBA_HOME/KubeArmor
+go test -v
+
+cd $KBA_HOME/LogServer
+go test -v
+
+exit 0
--- .travis/prepare.sh
+++ .travis/prepare.sh
@@ -13,11 +13,14 @@ sudo microk8s kubectl get nodes
 sudo microk8s kubectl get services
 
 # copy k8s config
+mkdir $HOME/.kube
 sudo microk8s kubectl config view --raw | sudo tee $HOME/.kube/config
 sudo chown -R travis: $HOME/.kube/
 
 # download kubectl
-curl -Lo kubectl https://storage.googleapis.com/kubernetes-release/release/v1.18.1/bin/linux/amd64/kubectl && chmod +x kubectl && sudo mv kubectl /usr/local/bin/
+curl -Lo kubectl https://storage.googleapis.com/kubernetes-release/release/v1.18.1/bin/linux/amd64/kubectl
+chmod +x kubectl
+sudo mv kubectl /usr/local/bin/
 
 # check kubectl
 kubectl cluster-info
