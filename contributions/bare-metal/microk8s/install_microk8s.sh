4c776b819d55336863e44e50afd9f09eee3af23f
Jaehyun Nam <jaehyun.nam@outlook.com>
2020-12-18 09:58:03 +0000

    minor fix

--- contributions/bare-metal/microk8s/install_microk8s.sh
+++ contributions/bare-metal/microk8s/install_microk8s.sh
@@ -13,7 +13,7 @@ sudo microk8s kubectl get nodes
 sudo microk8s kubectl get services
 
 # copy k8s config
-mkdir $HOME/.kube
+mkdir -p $HOME/.kube
 sudo microk8s kubectl config view --raw | sudo tee $HOME/.kube/config
 sudo chown -R $USER: $HOME/.kube/
 
