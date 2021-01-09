1ba19fbaa9dc1cecf3e99f42cd8649b2a9659365
Jaehyun Nam <jaehyun.nam@outlook.com>
2021-01-09 12:20:54 +0000

    minor update

--- contributions/build_and_push.sh
+++ contributions/build_and_push.sh
@@ -14,19 +14,6 @@ cd $KBA_HOME/KubeArmor/build
 ./build_kubearmor.sh $1
 ./push_kubearmor.sh $1
 
-./build_kubearmor.sh latest
-./push_kubearmor.sh latest
-
-# KubeArmor-COS-Auditd for GKE
-
-cd $KBA_HOME/GKE/cos-auditd/build
-
-./build_auditd.sh $1
-./push_auditd.sh $1
-
-./build_auditd.sh latest
-./push_auditd.sh latest
-
 # KubeArmor-LogServer
 
 cd $KBA_HOME/LogServer/build
@@ -34,5 +21,9 @@ cd $KBA_HOME/LogServer/build
 ./build_logserver.sh $1
 ./push_logserver.sh $1
 
-./build_logserver.sh latest
-./push_logserver.sh latest
+# KubeArmor-COS-Auditd for GKE
+
+cd $KBA_HOME/GKE/cos-auditd/build
+
+./build_auditd.sh $1
+./push_auditd.sh $1
