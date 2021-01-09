9852266112e87f65a5c178951deacb66a7f9ede5
Jaehyun Nam <jaehyun.nam@outlook.com>
2021-01-09 12:16:31 +0000

    updated LogServer

--- LogServer/build/build_logserver.sh
+++ LogServer/build/build_logserver.sh
@@ -1,46 +1,50 @@
 #!/bin/bash
 
-SRV_HOME=`dirname $(realpath "$0")`/..
-cd $SRV_HOME/build
+SERVER_PATH=`dirname $(realpath "$0")`/..
+cd $SERVER_PATH/build
+
+# check version
+
+VERSION=latest
+
+if [ ! -z $1 ]; then
+    VERSION=$1
+fi
 
 # remove old images
 
-docker images | grep kubearmor-logserver | awk '{print $3}' | xargs -I {} docker rmi -f {} 2> /dev/null
+docker images | grep accuknox/kubearmor-logserver | awk '{print $3}' | xargs -I {} docker rmi -f {} 2> /dev/null
 
-echo "[INFO] Removed existing KubeArmor images"
+echo "[INFO] Removed existing accuknox/kubearmor-logserver images"
 
 # remove old files (just in case)
 
-$SRV_HOME/build/clean_source_files.sh
+$SERVER_PATH/build/clean_source_files.sh
 
 echo "[INFO] Removed source files just in case"
 
 # copy files to build
 
-$SRV_HOME/build/copy_source_files.sh
+$SERVER_PATH/build/copy_source_files.sh
 
 echo "[INFO] Copied new source files"
 
 # build a new image
 
-if [ -z $1 ]; then
-    echo "[INFO] Building accuknox/kubearmor-logserver:latest"
-    docker build -t accuknox/kubearmor-logserver:latest  . -f $SRV_HOME/build/Dockerfile.logserver
-else
-    echo "[INFO] Building accuknox/kubearmor-logserver:$1"
-    docker build -t accuknox/kubearmor-logserver:$1  . -f $SRV_HOME/build/Dockerfile.logserver
-fi
+echo "[INFO] Building accuknox/kubearmor-logserver:$VERSION"
+docker build -t accuknox/kubearmor-logserver:$VERSION  . -f $SERVER_PATH/build/Dockerfile.logserver
 
 if [ $? == 0 ]; then
-    echo "[PASSED] Built the KubeArmor-LogServer image"
+    echo "[PASSED] Built accuknox/kubearmor-logserver:$VERSION"
+    # exit 0
 else
-    echo "[FAILED] Failed to build the KubeArmor-LogServer image"
+    echo "[FAILED] Failed to build accuknox/kubearmor-logserver:$VERSION"
     exit 1
 fi
 
 # remove old files
 
-$SRV_HOME/build/clean_source_files.sh
+$SERVER_PATH/build/clean_source_files.sh
 
 echo "[INFO] Removed source files"
 exit 0
