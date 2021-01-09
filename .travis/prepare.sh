76ddf85dc9f30ccf5e4b5e02dfbbb45cba20d566
Jaehyun Nam <jaehyun.nam@outlook.com>
2021-01-09 12:17:52 +0000

    update travis-ci

--- .travis/prepare.sh
+++ .travis/prepare.sh
@@ -13,10 +13,6 @@ sudo apt-get install -y linux-headers-$(uname -r)
 # install microk8s
 sudo snap install microk8s --classic
 
-# check microk8s
-# sudo microk8s kubectl get nodes
-# sudo microk8s kubectl get services
-
 # copy k8s config
 mkdir -p $HOME/.kube
 sudo microk8s kubectl config view --raw | sudo tee $HOME/.kube/config
@@ -26,6 +22,3 @@ sudo chown -R travis: $HOME/.kube/
 curl -Lo kubectl https://storage.googleapis.com/kubernetes-release/release/v1.18.1/bin/linux/amd64/kubectl
 chmod +x kubectl
 sudo mv kubectl /usr/local/bin/
-
-# check kubectl
-# kubectl cluster-info
