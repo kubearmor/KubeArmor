0285746d382eb22358eafa742c8a59b3d13364f0
Jaehyun Nam <jaehyun.nam@outlook.com>
2021-01-10 14:13:18 +0000

    update LogServer

--- LogServer/main.go
+++ LogServer/main.go
@@ -3,9 +3,42 @@ package main
 import (
 	"flag"
 	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
 
 	"github.com/accuknox/KubeArmor/LogServer/server"
 )
+
+// StopChan Channel
+var StopChan chan struct{}
+
+// LogServer Handler
+var LogServer *server.LogServer
+
+// init Function
+func init() {
+	StopChan = make(chan struct{})
+}
+
+// ==================== //
+// == Signal Handler == //
+// ==================== //
+
+// GetOSSigChannel Function
+func GetOSSigChannel() chan os.Signal {
+	c := make(chan os.Signal, 1)
+
+	signal.Notify(c,
+		syscall.SIGKILL,
+		syscall.SIGHUP,
+		syscall.SIGINT,
+		syscall.SIGTERM,
+		syscall.SIGQUIT,
+		os.Interrupt)
+
+	return c
+}
 
 // ========== //
 // == Main == //
@@ -20,17 +53,17 @@ func main() {
 	port := fmt.Sprintf(":%s", *portPtr)
 
 	// start server
-	logServer := server.NewLogServer(port)
+	LogServer = server.NewLogServer(port)
 
 	// receive logs
-	go logServer.ReceiveLogs()
+	go LogServer.ReceiveLogs()
 
 	// listen for interrupt signals
-	sigChan := logServer.GetChan()
+	sigChan := GetOSSigChannel()
 	<-sigChan
 	fmt.Println("Got a signal to terminate the LogServer")
-	logServer.StopChan()
+	close(StopChan)
 
 	// stop server
-	logServer.DestroyLogServer()
+	LogServer.DestroyLogServer()
 }
