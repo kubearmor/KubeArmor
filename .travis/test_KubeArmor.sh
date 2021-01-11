1e60fcea0140be68be4cada89b6b06131e556c80
Jaehyun Nam <jaehyun.nam@outlook.com>
2021-01-11 14:20:02 +0000

    update travis-ci

--- .travis/test_KubeArmor.sh
+++ .travis/test_KubeArmor.sh
@@ -5,17 +5,11 @@ ARMOR_HOME=`dirname $(realpath "$0")`/..
 # move to KubeArmor
 cd $ARMOR_HOME/KubeArmor
 
-# compile KubeArmor
-make
-
 # test KubeArmor
 make test
 
 # move to LogServer
 cd $ARMOR_HOME/LogServer
-
-# compile LogServer
-make
 
 # test LogServer
 make test
