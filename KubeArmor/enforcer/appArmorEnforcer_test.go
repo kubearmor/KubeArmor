1d1c8b7684db5c05b16b3c5f5494c07ed59e0857
Jaehyun Nam <jaehyun.nam@outlook.com>
2021-01-11 12:38:39 +0000

    update unittests

--- KubeArmor/enforcer/appArmorEnforcer_test.go
+++ KubeArmor/enforcer/appArmorEnforcer_test.go
@@ -1,45 +1,65 @@
 package enforcer
 
 import (
-	"os"
-	"path/filepath"
 	"testing"
 )
 
 func TestAppArmorEnforcer(t *testing.T) {
-	// == //
+	// Create AppArmor Enforcer
 
-	// Get Home directory
-
-	t.Log("[INFO] Get Home directory")
-
-	dir, err := filepath.Abs(filepath.Dir(os.Args[0]))
-	if err != nil {
-		t.Errorf("[FAIL] Failed to get the home directory (%s)", err.Error())
+	enforcer := NewAppArmorEnforcer()
+	if enforcer == nil {
+		t.Log("[FAIL] Failed to create AppArmor Enforcer")
 		return
 	}
 
-	t.Log("[PASS] Got Home directory")
-
-	// == //
-
-	// Create AppArmor Enforcer
-
-	t.Log("[INFO] Create AppArmor Enforcer")
-
-	enforcer := NewAppArmorEnforcer(dir)
-
 	t.Log("[PASS] Created AppArmor Enforcer")
-
-	// == //
 
 	// Destroy AppArmor Enforcer
 
-	t.Log("[INFO] Destroy AppArmor Enforcer")
-
-	enforcer.DestroyAppArmorEnforcer()
+	if err := enforcer.DestroyAppArmorEnforcer(); err != nil {
+		t.Log("[FAIL] Failed to destroy AppArmor Enforcer")
+		return
+	}
 
 	t.Log("[PASS] Destroyed AppArmor Enforcer")
+}
 
-	// == //
+func TestAppArmorProfile(t *testing.T) {
+	// Create AppArmor Enforcer
+
+	enforcer := NewAppArmorEnforcer()
+	if enforcer == nil {
+		t.Log("[FAIL] Failed to create AppArmor Enforcer")
+		return
+	}
+
+	t.Log("[PASS] Created AppArmor Enforcer")
+
+	// Register AppArmorProfile
+
+	if ok := enforcer.RegisterAppArmorProfile("test-profile"); !ok {
+		t.Error("[FAIL] Failed to register AppArmorProfile")
+		return
+	}
+
+	t.Log("[PASS] Registered AppArmorProfile")
+
+	// Unregister AppArmorProfile
+
+	if ok := enforcer.UnregisterAppArmorProfile("test-profile"); !ok {
+		t.Error("[FAIL] Failed to unregister AppArmorProfile")
+		return
+	}
+
+	t.Log("[PASS] Unregister AppArmorProfile")
+
+	// Destroy AppArmor Enforcer
+
+	if err := enforcer.DestroyAppArmorEnforcer(); err != nil {
+		t.Log("[FAIL] Failed to destroy AppArmor Enforcer")
+		return
+	}
+
+	t.Log("[PASS] Destroyed AppArmor Enforcer")
 }
