f310fcff2bde9e953ee50aa3c9b9c7812aa2892c
Jaehyun Nam <jaehyun.nam@outlook.com>
2021-01-11 14:20:02 +0000

    update travis-ci

--- .travis/prepare.sh
+++ .travis/prepare.sh
@@ -4,15 +4,6 @@
 sudo apt-get update
 sudo apt-get install -y linux-headers-$(uname -r)
 
-# install bcc
-sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 4052245BD4284CDD
-echo "deb https://repo.iovisor.org/apt/$(lsb_release -cs) $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/iovisor.list
-sudo apt-get update
-sudo apt-get install -y gcc make wget unzip bcc-tools libbcc-examples
-
-# install apparmor and audit
-sudo apt-get install -y apparmor apparmor-utils auditd
-
 # install microk8s
 sudo snap install microk8s --classic
 
