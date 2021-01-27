07c03b06dfbbedcf8139dde38f2d4d0d2e9039d1
Jaehyun Nam <jaehuyn.nam@outlook.com>
2021-01-27 03:40:34 +0000

    remove unused parts

--- .travis/verify_image.sh
+++ .travis/verify_image.sh
@@ -19,25 +19,5 @@ if [ "$IMAGE_NAME" != "$TEST_IMAGE" ]; then
     exit 1
 else
     echo "[PASSED] Built $IMAGE_NAME"
-    # exit 0
-fi
-
-# build KubeArmor-LogServer
-
-$ARMOR_HOME/LogServer/build/build_logserver.sh test
-
-# check KubeArmor-LogServer
-
-IMAGE_NAME="accuknox/kubearmor-logserver:test"
-TEST_IMAGE=`docker images --format '{{.Repository}}:{{.Tag}}' | grep $IMAGE_NAME`
-
-echo "Check KubeArmor-LogServer Image"
-echo ">> Expected:" $IMAGE_NAME ", Received:" $TEST_IMAGE
-
-if [ "$IMAGE_NAME" != "$TEST_IMAGE" ]; then
-    echo "[FAILED] Not built $IMAGE_NAME"
-    exit 1
-else
-    echo "[PASSED] Built $IMAGE_NAME"
     exit 0
 fi
