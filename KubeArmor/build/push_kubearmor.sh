2585a4a8e181efe46c72b19da9312c33ccc36726
Jaehyun Nam <jaehyun.nam@outlook.com>
2021-02-17 10:18:50 +0000

    update builds

--- KubeArmor/build/push_kubearmor.sh
+++ KubeArmor/build/push_kubearmor.sh
@@ -13,10 +13,10 @@ fi
 echo "[INFO] Pushing accuknox/kubearmor:$VERSION"
 docker push accuknox/kubearmor:$VERSION
 
-if [ $? == 0 ]; then
-    echo "[PASSED] Pushed accuknox/kubearmor:$VERSION"
-    exit 0
-else
+if [ $? != 0 ]; then
     echo "[FAILED] Failed to push accuknox/kubearmor:$VERSION"
     exit 1
+else
+    echo "[PASSED] Pushed accuknox/kubearmor:$VERSION"
+    exit 0
 fi
