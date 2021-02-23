f349e7f6d40b263c92f0c60cd1e16f5446807085
Jaehyun Nam <jn@accuknox.com>
2021-02-23 15:47:16 +0900

    Update verify_image.sh

--- .travis/verify_image.sh
+++ .travis/verify_image.sh
@@ -4,11 +4,16 @@ ARMOR_HOME=`dirname $(realpath "$0")`/..
 
 # build KubeArmor image
 
+IMAGE_NAME="accuknox/kubearmor:test"
+
 $ARMOR_HOME/KubeArmor/build/build_kubearmor.sh test
+if [ $? != 0 ]; then
+    echo "[FAILED] Failed to build $IMAGE_NAME"
+    exit 1
+fi
 
 # check KubeArmor image
 
-IMAGE_NAME="accuknox/kubearmor:test"
 TEST_IMAGE=`docker images --format '{{.Repository}}:{{.Tag}}' | grep $IMAGE_NAME`
 
 echo "Check KubeArmor Image"
