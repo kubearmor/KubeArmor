2acd04275dddd57783ad45b0f4970d8c9bc4de67
Jaehyun Nam <jaehyun.nam@outlook.com>
2021-05-25 09:49:14 +0000

    add clients inside of KubeArmor

--- KubeArmor/build/clean_source_files.sh
+++ KubeArmor/build/clean_source_files.sh
@@ -5,3 +5,7 @@ ARMOR_HOME=`dirname $(realpath "$0")`/..
 rm -rf $ARMOR_HOME/build/KubeArmor
 rm -rf $ARMOR_HOME/build/GKE
 rm -rf $ARMOR_HOME/build/protobuf
+
+rm -rf $ARMOR_HOME/build/LogClient
+rm -rf $ARMOR_HOME/build/MySQLClient
+rm -rf $ARMOR_HOME/build/KafkaClient
