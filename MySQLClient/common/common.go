718d1a84a5ed37793751d1e08aa1004765c82141
Jaehyun Nam <jaehyun.nam@outlook.com>
2021-05-28 15:31:33 +0000

    update MySQLClient

--- MySQLClient/common/common.go
+++ MySQLClient/common/common.go
@@ -10,7 +10,7 @@ func StrToFile(str, destFile string) {
 	if _, err := os.Stat(destFile); err != nil {
 		newFile, err := os.Create(destFile)
 		if err != nil {
-			fmt.Errorf("Failed to create a file (%s, %s)", destFile, err.Error())
+			fmt.Printf("Failed to create a file (%s, %s)\n", destFile, err.Error())
 			return
 		}
 		newFile.Close()
@@ -18,12 +18,12 @@ func StrToFile(str, destFile string) {
 
 	file, err := os.OpenFile(destFile, os.O_WRONLY|os.O_APPEND, 0644)
 	if err != nil {
-		fmt.Errorf("Failed to open a file (%s, %s)", destFile, err.Error())
+		fmt.Printf("Failed to open a file (%s, %s)\n", destFile, err.Error())
 	}
 	defer file.Close()
 
 	_, err = file.WriteString(str)
 	if err != nil {
-		fmt.Errorf("Failed to write a string into the file (%s, %s)", destFile, err.Error())
+		fmt.Printf("Failed to write a string into the file (%s, %s)\n", destFile, err.Error())
 	}
 }
--- MySQLClient/main.go
+++ MySQLClient/main.go
@@ -48,11 +48,29 @@ func GetOSSigChannel() chan os.Signal {
 func main() {
 	// == //
 
-	gRPCPtr := flag.String("gRPC", "localhost:32767", "gRPC server information")
+	gRPCPtr := flag.String("gRPC", "", "gRPC server information")
 	msgPathPtr := flag.String("msgPath", "none", "Output location for messages, {path|stdout|none}")
 	logPathPtr := flag.String("logPath", "none", "Output location for alerts and logs, {path|stdout|none}")
 	dropTablePtr := flag.Bool("dropTables", false, "Flag to drop the existing tables")
 	flag.Parse()
+
+	// == //
+
+	gRPC := ""
+
+	fmt.Println("== KubeArmor information ==")
+
+	if *gRPCPtr != "" {
+		gRPC = *gRPCPtr
+	} else {
+		if val, ok := os.LookupEnv("KUBEARMOR_SERVICE"); ok {
+			gRPC = val
+		} else {
+			gRPC = "localhost:32767"
+		}
+	}
+
+	fmt.Println("  gRPC server: " + gRPC)
 
 	// == //
 
@@ -70,7 +88,7 @@ func main() {
 		dbHost = val[6:]
 		fmt.Println("  DB_HOST:     " + dbHost)
 	} else {
-		fmt.Errorf("Failed to get DB_HOST from env")
+		fmt.Println("Failed to get DB_HOST from env")
 		return
 	}
 
@@ -78,7 +96,7 @@ func main() {
 		dbName = val
 		fmt.Println("  DB_NAME:     " + dbName)
 	} else {
-		fmt.Errorf("Failed to get DB_NAME from env")
+		fmt.Println("Failed to get DB_NAME from env")
 		return
 	}
 
@@ -86,7 +104,7 @@ func main() {
 		dbUser = val
 		fmt.Println("  DB_USER:     " + dbUser)
 	} else {
-		fmt.Errorf("Failed to get DB_USER from env")
+		fmt.Println("Failed to get DB_USER from env")
 		return
 	}
 
@@ -94,7 +112,7 @@ func main() {
 		dbPasswd = val
 		fmt.Println("  DB_PASSWD:   ********")
 	} else {
-		fmt.Errorf("Failed to get DB_PASSWD from env")
+		fmt.Println("Failed to get DB_PASSWD from env")
 		return
 	}
 
@@ -118,7 +136,7 @@ func main() {
 	}
 
 	if dbMsgTable == "" && dbAlertTable == "" && dbLogTable == "" {
-		fmt.Errorf("Failed to get some of TABLE_MSG, TABLE_ALERT, and TABLE_LOG")
+		fmt.Println("Failed to get some of TABLE_MSG, TABLE_ALERT, and TABLE_LOG")
 		return
 	}
 
@@ -126,7 +144,7 @@ func main() {
 
 	db, err := sql.Open("mysql", dbUser+":"+dbPasswd+"@tcp("+dbHost+")/"+dbName)
 	if err != nil {
-		fmt.Errorf("Failed to connect to the MySQL database (%s)", err.Error())
+		fmt.Printf("Failed to connect to the MySQL database (%s)\n", err.Error())
 		return
 	}
 	db.Close()
@@ -136,7 +154,7 @@ func main() {
 	if *dropTablePtr {
 		db, err := sql.Open("mysql", dbUser+":"+dbPasswd+"@tcp("+dbHost+")/"+dbName)
 		if err != nil {
-			fmt.Errorf("Failed to connect to the MySQL database (%s)", err.Error())
+			fmt.Printf("Failed to connect to the MySQL database (%s)\n", err.Error())
 			return
 		}
 
@@ -144,7 +162,7 @@ func main() {
 			query := "DROP TABLE IF EXISTS `" + dbMsgTable + "`;"
 
 			if _, err := db.Query(query); err != nil {
-				fmt.Errorf("Failed to drop %s (%s)", dbMsgTable, err.Error())
+				fmt.Printf("Failed to drop %s (%s)\n", dbMsgTable, err.Error())
 				return
 			}
 
@@ -155,7 +173,7 @@ func main() {
 			query := "DROP TABLE IF EXISTS `" + dbAlertTable + "`;"
 
 			if _, err := db.Query(query); err != nil {
-				fmt.Errorf("Failed to drop %s (%s)", dbAlertTable, err.Error())
+				fmt.Printf("Failed to drop %s (%s)\n", dbAlertTable, err.Error())
 				return
 			}
 
@@ -166,7 +184,7 @@ func main() {
 			query := "DROP TABLE IF EXISTS `" + dbLogTable + "`;"
 
 			if _, err := db.Query(query); err != nil {
-				fmt.Errorf("Failed to drop %s (%s)", dbLogTable, err.Error())
+				fmt.Printf("Failed to drop %s (%s)\n", dbLogTable, err.Error())
 				return
 			}
 
@@ -181,18 +199,18 @@ func main() {
 	// == //
 
 	// create a client
-	logClient := core.NewClient(*gRPCPtr, dbHost, dbName, dbUser, dbPasswd, dbMsgTable, dbAlertTable, dbLogTable)
+	logClient := core.NewClient(gRPC, dbHost, dbName, dbUser, dbPasswd, dbMsgTable, dbAlertTable, dbLogTable)
 	if logClient == nil {
-		fmt.Errorf("Failed to create a gRPC client (%s)", *gRPCPtr)
+		fmt.Printf("Failed to create a gRPC client (%s)\n", gRPC)
 		return
 	}
-	fmt.Printf("Created a gRPC client (%s)\n", *gRPCPtr)
+	fmt.Printf("Created a gRPC client (%s)\n", gRPC)
 
 	// create DB tables
 	if ok := logClient.CreateTablesIfNotExist(); !ok {
 		// destroy the client
 		if err := logClient.DestroyClient(); err != nil {
-			fmt.Errorf("Failed to destroy the gRPC client (%s)", err.Error())
+			fmt.Printf("Failed to destroy the gRPC client (%s)\n", err.Error())
 			return
 		}
 		fmt.Println("Destroyed the gRPC client")
@@ -201,7 +219,7 @@ func main() {
 
 	// do healthcheck
 	if ok := logClient.DoHealthCheck(); !ok {
-		fmt.Errorf("Failed to check the liveness of the gRPC server")
+		fmt.Println("Failed to check the liveness of the gRPC server")
 		return
 	}
 	fmt.Println("Checked the liveness of the gRPC server")
@@ -231,7 +249,7 @@ func main() {
 
 	// destroy the client
 	if err := logClient.DestroyClient(); err != nil {
-		fmt.Errorf("Failed to destroy the gRPC client (%s)", err.Error())
+		fmt.Printf("Failed to destroy the gRPC client (%s)\n", err.Error())
 		return
 	}
 	fmt.Println("Destroyed the gRPC client")
