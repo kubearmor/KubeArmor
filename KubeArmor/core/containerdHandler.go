kubearmor/KubeArmor

commit 3d55d92a72ffdccf512c25066dd481d9dc0a1baa

Add function to get already-deployed containerInfo in dockerHandler
This change would be used for older versions of Docker(v18.x and earlier).

Fixes: #163

Signed-off-by: Ayush Dwivedi <ayush.dwivedi@accuknox.com>

KubeArmor/core/dockerHandler.go
@@ -140,6 +140,36 @@ func (dh *DockerHandler) GetEventChannel() <-chan events.Message {
 // == Docker Events == //
 // =================== //
 
+// GetAlreadyDeployedDockerContainers Function
+func (dm *KubeArmorDaemon) GetAlreadyDeployedDockerContainers() {
+	if containerList, err := Docker.DockerClient.ContainerList(context.Background(), types.ContainerListOptions{}); err == nil {
+		for _, dcontainer := range containerList {
+			// get container information from docker client
+			container, err := Docker.GetContainerInfo(dcontainer.ID)
+			if err != nil {
+				continue
+			}
+
+			if container.ContainerID == "" {
+				continue
+			}
+
+			if dcontainer.State == "running" {
+				dm.ContainersLock.Lock()
+				if _, ok := dm.Containers[container.ContainerID]; !ok {
+					dm.Containers[container.ContainerID] = container
+				} else {
+					dm.ContainersLock.Unlock()
+					continue
+				}
+				dm.ContainersLock.Unlock()
+
+				dm.LogFeeder.Printf("Detected a container (added/%s)", container.ContainerID[:12])
+			}
+		}
+	}
+}
+
 // UpdateDockerContainer Function
 func (dm *KubeArmorDaemon) UpdateDockerContainer(containerID, action string) {
 	container := tp.Container{}
@@ -166,7 +196,7 @@ func (dm *KubeArmorDaemon) UpdateDockerContainer(containerID, action string) {
 		}
 		dm.ContainersLock.Unlock()
 
-		dm.LogFeeder.Printf("Detected a container (added/%s)", container.ContainerID[:12])
+		dm.LogFeeder.Printf("Detected a container (added/%s)", containerID[:12])
 
 	} else if action == "stop" || action == "destroy" {
 		// case 1: kill -> die -> stop

KubeArmor/core/kubeArmor.go
@@ -443,6 +443,9 @@ func KubeArmor(clusterName, gRPCPort, logPath string, enableAuditd, enableHostPo
 				}
 
 				if sockFile {
+					// update already deployed containers
+					dm.GetAlreadyDeployedDockerContainers()
+
 					// monitor docker events
 					go dm.MonitorDockerEvents()
 				} else {

commit ed74383cfd9362d779449880797f2b796fcc42e9

Fix minor bug in containerdHandler and dockerHandler
Update AppArmorProfiles for pods detected prior to container monitor start.

Fixes: 3d55d92a72ff

Signed-off-by: Ayush Dwivedi <ayush.dwivedi@accuknox.com>

KubeArmor/core/containerdHandler.go
@@ -217,6 +217,8 @@ func (dm *KubeArmorDaemon) UpdateContainerdContainer(ctx context.Context, contai
 		dm.ContainersLock.Lock()
 		if _, ok := dm.Containers[containerID]; !ok {
 			dm.Containers[containerID] = container
+		} else if dm.Containers[containerID].AppArmorProfile == "" && container.AppArmorProfile != "" {
+			dm.Containers[containerID] = container
 		} else {
 			dm.ContainersLock.Unlock()
 			return
@@ -251,6 +253,8 @@ func (dm *KubeArmorDaemon) MonitorContainerdEvents() {
 
 	dm.LogFeeder.Print("Started to monitor Containerd events")
 
+	getContainersExecutedOnce := false
+
 	for {
 		select {
 		case <-StopChan:
@@ -276,6 +280,20 @@ func (dm *KubeArmorDaemon) MonitorContainerdEvents() {
 				for containerID, context := range deletedContainers {
 					dm.UpdateContainerdContainer(context, containerID, "destroy")
 				}
+			}
+
+			if !getContainersExecutedOnce {
+				for _, conGroup := range dm.ContainerGroups {
+					for _, containerID := range conGroup.Containers {
+						if container, ok := dm.Containers[containerID]; ok {
+							if conGroup.AppArmorProfiles[containerID] == "" {
+								conGroup.AppArmorProfiles[containerID] = container.AppArmorProfile
+							}
+						}
+					}
+				}
+
+				getContainersExecutedOnce = true
 			}
 		}
 

KubeArmor/core/dockerHandler.go
@@ -158,6 +158,8 @@ func (dm *KubeArmorDaemon) GetAlreadyDeployedDockerContainers() {
 				dm.ContainersLock.Lock()
 				if _, ok := dm.Containers[container.ContainerID]; !ok {
 					dm.Containers[container.ContainerID] = container
+				} else if dm.Containers[container.ContainerID].AppArmorProfile == "" && container.AppArmorProfile != "" {
+					dm.Containers[container.ContainerID] = container
 				} else {
 					dm.ContainersLock.Unlock()
 					continue
@@ -165,6 +167,16 @@ func (dm *KubeArmorDaemon) GetAlreadyDeployedDockerContainers() {
 				dm.ContainersLock.Unlock()
 
 				dm.LogFeeder.Printf("Detected a container (added/%s)", container.ContainerID[:12])
+			}
+		}
+	}
+
+	for _, conGroup := range dm.ContainerGroups {
+		for _, containerID := range conGroup.Containers {
+			if container, ok := dm.Containers[containerID]; ok {
+				if conGroup.AppArmorProfiles[containerID] == "" {
+					conGroup.AppArmorProfiles[containerID] = container.AppArmorProfile
+				}
 			}
 		}
 	}