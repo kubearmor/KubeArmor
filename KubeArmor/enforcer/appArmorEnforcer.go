cdb7f3587792e4e0b94f38b974b47c3dea49411a
Jaehyun Nam <jaehyun.nam@outlook.com>
2021-07-09 06:48:53 +0000

    update HostProfile

--- KubeArmor/enforcer/appArmorEnforcer.go
+++ KubeArmor/enforcer/appArmorEnforcer.go
@@ -301,6 +301,7 @@ func (ae *AppArmorEnforcer) CreateAppArmorHostProfile() error {
 		"\n" +
 		"  /usr/bin/runc Ux,\n" + // docker
 		"  /usr/sbin/runc Ux,\n" + // containerd
+		"  /snap/microk8s/2262/bin/runc Ux,\n" + // microk8s
 		"  /snap/microk8s/2264/bin/runc Ux,\n" + // microk8s
 		"\n" +
 		"  ## == POLICY START == ##\n" +
