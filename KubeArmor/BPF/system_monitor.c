kubearmor/KubeArmor

commit 98b68130a855d45bd296c97d02fffff5aa309aab

Enable already-deployed container monitoring in system_monitor
This commit makes it possible to have already-deployed container entries in pid_ns_map.
This will enable log generation for these containers.

Fixes: #163

Signed-off-by: Ayush Dwivedi <ayush.dwivedi@accuknox.com>

KubeArmor/BPF/system_monitor.c
@@ -182,12 +182,8 @@ static __always_inline u32 add_pid_ns()
         return pid_ns;
     }
 
-    if (get_task_ns_pid(task) == 1) {
-        pid_ns_map.update(&pid_ns, &one);
-        return pid_ns;
-    }
-
-    return 0;
+    pid_ns_map.update(&pid_ns, &one);
+    return pid_ns;
 
 #endif /* MONITOR_HOST || MONITOR_CONTAINER */
 }

commit f25af12f45047c7b7c96193fa1297ead9c023b40

Fix condition for already-deployed containers in system_monitor.c
Fixes: #163
Fixes: 98b68130a855

Signed-off-by: Ayush Dwivedi <ayush.dwivedi@accuknox.com>

KubeArmor/BPF/system_monitor.c
@@ -179,16 +179,17 @@ static __always_inline u32 add_pid_ns()
 #else /* MONITOR_CONTAINER */
 
     u32 pid_ns = get_task_pid_ns_id(task);
+    
+    // discard host event 
+    if (pid_ns == PROC_PID_INIT_INO) {
+        return 0;
+    }
+
     if (pid_ns_map.lookup(&pid_ns) != 0) {
         return pid_ns;
-    }
-
-    if (get_task_ns_pid(task) == 1) {
-        pid_ns_map.update(&pid_ns, &one);
-        return pid_ns;
-    }
-
-    return 0;
+    } 
+    pid_ns_map.update(&pid_ns, &one);
+    return pid_ns;
 
 #endif /* MONITOR_HOST || MONITOR_CONTAINER */
 }
@@ -213,6 +214,11 @@ static __always_inline u32 remove_pid_ns()
 #else /* !MONITOR_HOST */
 
     u32 pid_ns = get_task_pid_ns_id(task);
+
+    if (pid_ns == PROC_PID_INIT_INO) {
+        return 0;
+    }
+
     if (pid_ns_map.lookup(&pid_ns) != 0 && (get_task_ns_pid(task) == 1)) {
         pid_ns_map.delete(&pid_ns);
         return 0;