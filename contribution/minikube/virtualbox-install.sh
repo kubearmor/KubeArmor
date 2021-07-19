a7014f5ee63fbe3f1e69c01255b966240ef0fa19
Jaehyun Nam <jaehyun.nam@outlook.com>
2021-07-19 14:48:24 +0000

    update minikube scripts

---
+++ contribution/minikube/virtualbox-install.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# install virtualbox
+wget -q https://www.virtualbox.org/download/oracle_vbox_2016.asc -O- | sudo apt-key add -
+wget -q https://www.virtualbox.org/download/oracle_vbox.asc -O- | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] http://download.virtualbox.org/virtualbox/debian $(lsb_release -cs) contrib"
+sudo apt-get update
+sudo apt-get -y install virtualbox-6.1
