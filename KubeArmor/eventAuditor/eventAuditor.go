7bfc37abe86c98f773e0f552c4d0990060bc5bb9
Jaehyun Nam <jaehyun.nam@outlook.com>
2021-07-20 14:34:25 +0000

    add comments

--- KubeArmor/eventAuditor/eventAuditor.go
+++ KubeArmor/eventAuditor/eventAuditor.go
@@ -10,8 +10,12 @@ type EventAuditor struct {
 }
 
 // NewEventAuditor Function
-func NewEventAuditor() *EventAuditor {
+func NewEventAuditor() *EventAuditor { // (auditPolicies map[xxx]yyy, ...)
 	ea := &EventAuditor{}
+
+	// structure pointer for audit policies
+	// assume that all macros are already applied to audit policies
+
 	return ea
 }
 
@@ -19,3 +23,34 @@ func NewEventAuditor() *EventAuditor {
 func (ea *EventAuditor) DestroyEventAuditor() error {
 	return nil
 }
+
+// ============================= //
+// == Audit Policy Management == //
+// ============================= //
+
+// UpdateAuditPolicies Function
+func (ea *EventAuditor) UpdateAuditPolicies() { // (action string, auditPolicy yyy, ...)
+	// update audit policies
+
+	// call "entrypoint management"
+	// call "shared map management"
+}
+
+// =========================== //
+// == Entrypoint Management == //
+// =========================== //
+
+// handle kprobes, tracepoints
+// handle entrypoint
+
+// =========================== //
+// == Shared Map Management == //
+// =========================== //
+
+// handle process-spec table
+
+// ========================== //
+// == eBPF Code Generation == //
+// ========================== //
+
+// TBD
