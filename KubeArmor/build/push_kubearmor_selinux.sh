6413da388877137436b1403faef2250304ddfccc
Jaehyun Nam <jaehyun.nam@outlook.com>
2021-07-21 14:43:55 +0000

    update Dockerfile

---
+++ KubeArmor/build/push_kubearmor_selinux.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# check version
+
+VERSION=latest
+
+if [ ! -z $1 ]; then
+    VERSION=$1
+fi
+
+# push kubearmor/kubearmor-selinux
+
+echo "[INFO] Pushing kubearmor/kubearmor-selinux:$VERSION"
+docker push kubearmor/kubearmor-selinux:$VERSION
+
+if [ $? != 0 ]; then
+    echo "[FAILED] Failed to push kubearmor/kubearmor-selinux:$VERSION"
+    exit 1
+else
+    echo "[PASSED] Pushed kubearmor/kubearmor-selinux:$VERSION"
+    exit 0
+fi
