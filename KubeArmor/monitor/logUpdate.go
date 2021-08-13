0a37f4e6912e66abea81e7d6c0b7234dd74dc744
Jaehyun Nam <jaehyun.nam@outlook.com>
2021-08-13 16:57:15 +0000

    update system monitor

---
+++ KubeArmor/monitor/logUpdate.go
@@ -0,0 +1,282 @@
+// Copyright 2021 Authors of KubeArmor
+// SPDX-License-Identifier: Apache-2.0
+
+package monitor
+
+import (
+	"bytes"
+	"fmt"
+	"strconv"
+
+	kl "github.com/kubearmor/KubeArmor/KubeArmor/common"
+	tp "github.com/kubearmor/KubeArmor/KubeArmor/types"
+)
+
+// ========== //
+// == Logs == //
+// ========== //
+
+// UpdateContainerInfoByContainerID Function
+func (mon *SystemMonitor) UpdateContainerInfoByContainerID(log tp.Log) tp.Log {
+	Containers := *(mon.Containers)
+	ContainersLock := *(mon.ContainersLock)
+
+	ContainersLock.RLock()
+	defer ContainersLock.RUnlock()
+
+	if val, ok := Containers[log.ContainerID]; ok {
+		// update container info
+		log.NamespaceName = val.NamespaceName
+		log.PodName = val.ContainerGroupName
+		log.ContainerName = val.ContainerName
+
+		// update policy flag
+		log.PolicyEnabled = val.PolicyEnabled
+
+		// update visibility flags
+		log.ProcessVisibilityEnabled = val.ProcessVisibilityEnabled
+		log.FileVisibilityEnabled = val.FileVisibilityEnabled
+		log.NetworkVisibilityEnabled = val.NetworkVisibilityEnabled
+		log.CapabilitiesVisibilityEnabled = val.CapabilitiesVisibilityEnabled
+	}
+
+	return log
+}
+
+// BuildLogBase Function
+func (mon *SystemMonitor) BuildLogBase(msg ContextCombined) tp.Log {
+	log := tp.Log{}
+
+	timestamp, updatedTime := kl.GetDateTimeNow()
+
+	log.Timestamp = timestamp
+	log.UpdatedTime = updatedTime
+	log.ContainerID = msg.ContainerID
+
+	if log.ContainerID != "" {
+		log = mon.UpdateContainerInfoByContainerID(log)
+	}
+
+	log.HostPID = int32(msg.ContextSys.HostPID)
+
+	log.PPID = int32(msg.ContextSys.PPID)
+	log.PID = int32(msg.ContextSys.PID)
+	log.UID = int32(msg.ContextSys.UID)
+
+	if msg.ContextSys.EventID == SysExecve || msg.ContextSys.EventID == SysExecveAt {
+		log.Source = mon.GetExecPath(msg.ContainerID, msg.ContextSys.PPID)
+	} else {
+		log.Source = mon.GetExecPath(msg.ContainerID, msg.ContextSys.PID)
+	}
+
+	if log.Source == "" {
+		log.Source = string(msg.ContextSys.Comm[:bytes.IndexByte(msg.ContextSys.Comm[:], 0)])
+	}
+
+	return log
+}
+
+// UpdateLogs Function
+func (mon *SystemMonitor) UpdateLogs() {
+	for {
+		select {
+		case <-StopChan:
+			return
+
+		case msg, valid := <-mon.ContextChan:
+			if !valid {
+				continue
+			}
+
+			// generate a log
+
+			log := mon.BuildLogBase(msg)
+
+			switch msg.ContextSys.EventID {
+			case SysOpen:
+				var fileName string
+				var fileOpenFlags string
+
+				if len(msg.ContextArgs) == 2 {
+					if val, ok := msg.ContextArgs[0].(string); ok {
+						fileName = val
+					}
+					if val, ok := msg.ContextArgs[1].(string); ok {
+						fileOpenFlags = val
+					}
+				}
+
+				log.Operation = "File"
+				log.Resource = fileName
+				log.Data = "syscall=" + getSyscallName(int32(msg.ContextSys.EventID)) + " flags=" + fileOpenFlags
+
+			case SysOpenAt:
+				var fd string
+				var fileName string
+				var fileOpenFlags string
+
+				if len(msg.ContextArgs) == 3 {
+					if val, ok := msg.ContextArgs[0].(int32); ok {
+						fd = strconv.Itoa(int(val))
+					}
+					if val, ok := msg.ContextArgs[1].(string); ok {
+						fileName = val
+					}
+					if val, ok := msg.ContextArgs[2].(string); ok {
+						fileOpenFlags = val
+					}
+				}
+
+				log.Operation = "File"
+				log.Resource = fileName
+				log.Data = "syscall=" + getSyscallName(int32(msg.ContextSys.EventID)) + " fd=" + fd + " flags=" + fileOpenFlags
+
+			case SysClose:
+				var fd string
+
+				if len(msg.ContextArgs) == 1 {
+					if val, ok := msg.ContextArgs[0].(int32); ok {
+						fd = strconv.Itoa(int(val))
+					}
+				}
+
+				log.Operation = "File"
+				log.Resource = ""
+				log.Data = "syscall=" + getSyscallName(int32(msg.ContextSys.EventID)) + " fd=" + fd
+
+			case SysSocket: // domain, type, proto
+				var sockDomain string
+				var sockType string
+				var sockProtocol string
+
+				if len(msg.ContextArgs) == 3 {
+					if val, ok := msg.ContextArgs[0].(string); ok {
+						sockDomain = val
+					}
+					if val, ok := msg.ContextArgs[1].(string); ok {
+						sockType = val
+					}
+					if val, ok := msg.ContextArgs[2].(int32); ok {
+						sockProtocol = strconv.Itoa(int(val))
+					}
+				}
+
+				log.Operation = "Network"
+				log.Resource = "domain=" + sockDomain + " type=" + sockType + " protocol=" + sockProtocol
+				log.Data = "syscall=" + getSyscallName(int32(msg.ContextSys.EventID))
+
+			case SysConnect: // fd, sockaddr
+				var fd string
+				var sockAddr map[string]string
+
+				if len(msg.ContextArgs) == 2 {
+					if val, ok := msg.ContextArgs[0].(int32); ok {
+						fd = strconv.Itoa(int(val))
+					}
+					if val, ok := msg.ContextArgs[1].(map[string]string); ok {
+						sockAddr = val
+					}
+				}
+
+				log.Operation = "Network"
+				log.Resource = ""
+
+				for k, v := range sockAddr {
+					if log.Resource == "" {
+						log.Resource = k + "=" + v
+					} else {
+						log.Resource = log.Resource + " " + k + "=" + v
+					}
+				}
+
+				log.Data = "syscall=" + getSyscallName(int32(msg.ContextSys.EventID)) + " fd=" + fd
+
+			case SysAccept: // fd, sockaddr
+				var fd string
+				var sockAddr map[string]string
+
+				if len(msg.ContextArgs) == 2 {
+					if val, ok := msg.ContextArgs[0].(int32); ok {
+						fd = strconv.Itoa(int(val))
+					}
+					if val, ok := msg.ContextArgs[1].(map[string]string); ok {
+						sockAddr = val
+					}
+				}
+
+				log.Operation = "Network"
+				log.Resource = ""
+				log.Data = "syscall=" + getSyscallName(int32(msg.ContextSys.EventID)) + " fd=" + fd
+
+				for k, v := range sockAddr {
+					if log.Resource == "" {
+						log.Resource = k + "=" + v
+					} else {
+						log.Resource = log.Resource + " " + k + "=" + v
+					}
+				}
+
+			case SysBind: // fd, sockaddr
+				var fd string
+				var sockAddr map[string]string
+
+				if len(msg.ContextArgs) == 2 {
+					if val, ok := msg.ContextArgs[0].(int32); ok {
+						fd = strconv.Itoa(int(val))
+					}
+					if val, ok := msg.ContextArgs[1].(map[string]string); ok {
+						sockAddr = val
+					}
+				}
+
+				log.Operation = "Network"
+				log.Resource = ""
+
+				for k, v := range sockAddr {
+					if log.Resource == "" {
+						log.Resource = k + "=" + v
+					} else {
+						log.Resource = log.Resource + " " + k + "=" + v
+					}
+				}
+
+				log.Data = "syscall=" + getSyscallName(int32(msg.ContextSys.EventID)) + " fd=" + fd
+
+			case SysListen: // fd
+				var fd string
+
+				if len(msg.ContextArgs) == 2 {
+					if val, ok := msg.ContextArgs[0].(int32); ok {
+						fd = strconv.Itoa(int(val))
+					}
+				}
+
+				log.Operation = "Network"
+				log.Resource = ""
+				log.Data = "syscall=" + getSyscallName(int32(msg.ContextSys.EventID)) + " fd=" + fd
+
+			default:
+				continue
+			}
+
+			// get error message
+
+			if msg.ContextSys.Retval < 0 {
+				message := getErrorMessage(msg.ContextSys.Retval)
+				if message != "" {
+					log.Result = message
+				} else {
+					log.Result = fmt.Sprintf("Unknown (%d)", msg.ContextSys.Retval)
+				}
+			} else {
+				log.Result = "Passed"
+			}
+
+			// push the generated log
+
+			if mon.Logger != nil {
+				go mon.Logger.PushLog(log)
+			}
+		}
+	}
+}
---
+++ KubeArmor/monitor/processTree.go
@@ -0,0 +1,212 @@
+// Copyright 2021 Authors of KubeArmor
+// SPDX-License-Identifier: Apache-2.0
+
+package monitor
+
+import (
+	"time"
+
+	tp "github.com/kubearmor/KubeArmor/KubeArmor/types"
+)
+
+// ============================ //
+// == PID-to-ContainerID Map == //
+// ============================ //
+
+// LookupContainerID Function
+func (mon *SystemMonitor) LookupContainerID(pidns, mntns, ppid, pid uint32) string {
+	key := NsKey{PidNS: pidns, MntNS: mntns}
+
+	mon.NsMapLock.RLock()
+	defer mon.NsMapLock.RUnlock()
+
+	if val, ok := mon.NsMap[key]; ok {
+		return val
+	}
+
+	return ""
+}
+
+// AddContainerIDToNsMap Function
+func (mon *SystemMonitor) AddContainerIDToNsMap(containerID string, pidns, mntns uint32) {
+	key := NsKey{PidNS: pidns, MntNS: mntns}
+
+	mon.NsMapLock.Lock()
+	defer mon.NsMapLock.Unlock()
+
+	mon.NsMap[key] = containerID
+}
+
+// DeleteContainerIDFromNsMap Function
+func (mon *SystemMonitor) DeleteContainerIDFromNsMap(containerID string) {
+	ns := NsKey{}
+
+	mon.NsMapLock.RLock()
+	defer mon.NsMapLock.RUnlock()
+
+	for key, val := range mon.NsMap {
+		if containerID == val {
+			ns = key
+			break
+		}
+	}
+
+	if ns.PidNS != 0 && ns.MntNS != 0 {
+		delete(mon.NsMap, ns)
+	}
+}
+
+// ================== //
+// == Process Tree == //
+// ================== //
+
+// BuildPidNode Function
+func (mon *SystemMonitor) BuildPidNode(ctx SyscallContext, execPath string, args []string) tp.PidNode {
+	node := tp.PidNode{}
+
+	node.HostPPID = ctx.HostPPID
+	node.HostPID = ctx.HostPID
+
+	node.PPID = ctx.PPID
+	node.PID = ctx.PID
+	node.UID = ctx.UID
+
+	node.Comm = string(ctx.Comm[:])
+	node.ExecPath = execPath
+
+	for idx, arg := range args {
+		if idx == 0 {
+			continue
+		} else {
+			node.ExecPath = node.ExecPath + " " + arg
+		}
+	}
+
+	node.Exited = false
+
+	return node
+}
+
+// AddActivePid Function
+func (mon *SystemMonitor) AddActivePid(containerID string, node tp.PidNode) {
+	ActivePidMap := *(mon.ActivePidMap)
+	ActivePidMapLock := *(mon.ActivePidMapLock)
+
+	ActivePidMapLock.Lock()
+	defer ActivePidMapLock.Unlock()
+
+	// add pid node to ActivePidMap
+	if pidMap, ok := ActivePidMap[containerID]; ok {
+		pidMap[node.PID] = node
+	} else {
+		newPidMap := tp.PidMap{node.PID: node}
+		ActivePidMap[containerID] = newPidMap
+	}
+
+	ActiveHostPidMap := *(mon.ActiveHostPidMap)
+
+	// add pid node to ActiveHostPidMap
+	if pidMap, ok := ActiveHostPidMap[containerID]; ok {
+		pidMap[node.HostPID] = node
+	} else {
+		newPidMap := tp.PidMap{node.HostPID: node}
+		ActiveHostPidMap[containerID] = newPidMap
+	}
+}
+
+// GetExecPath Function
+func (mon *SystemMonitor) GetExecPath(containerID string, pid uint32) string {
+	ActivePidMap := *(mon.ActivePidMap)
+	ActivePidMapLock := *(mon.ActivePidMapLock)
+
+	ActivePidMapLock.RLock()
+	defer ActivePidMapLock.RUnlock()
+
+	if pidMap, ok := ActivePidMap[containerID]; ok {
+		if node, ok := pidMap[pid]; ok {
+			return node.ExecPath
+		}
+	}
+
+	return ""
+}
+
+// GetExecPathWithHostPID Function
+func (mon *SystemMonitor) GetExecPathWithHostPID(containerID string, hostPid uint32) string {
+	ActiveHostPidMap := *(mon.ActiveHostPidMap)
+	ActivePidMapLock := *(mon.ActivePidMapLock)
+
+	ActivePidMapLock.RLock()
+	defer ActivePidMapLock.RUnlock()
+
+	if pidMap, ok := ActiveHostPidMap[containerID]; ok {
+		if node, ok := pidMap[hostPid]; ok {
+			return node.ExecPath
+		}
+	}
+
+	return ""
+}
+
+// DeleteActivePid Function
+func (mon *SystemMonitor) DeleteActivePid(containerID string, ctx SyscallContext) {
+	ActivePidMap := *(mon.ActivePidMap)
+	ActivePidMapLock := *(mon.ActivePidMapLock)
+
+	ActivePidMapLock.Lock()
+	defer ActivePidMapLock.Unlock()
+
+	// delete execve(at) pid
+	if pidMap, ok := ActivePidMap[containerID]; ok {
+		if node, ok := pidMap[ctx.PID]; ok {
+			node.Exited = true
+			node.ExitedTime = time.Now()
+		}
+	}
+
+	ActiveHostPidMap := *(mon.ActiveHostPidMap)
+
+	// delete execve(at) host pid
+	if pidMap, ok := ActiveHostPidMap[containerID]; ok {
+		if node, ok := pidMap[ctx.HostPID]; ok {
+			node.Exited = true
+			node.ExitedTime = time.Now()
+		}
+	}
+}
+
+// CleanUpExitedHostPids Function
+func (mon *SystemMonitor) CleanUpExitedHostPids() {
+	for range mon.Ticker.C {
+		now := time.Now()
+
+		ActivePidMap := *(mon.ActivePidMap)
+		ActivePidMapLock := *(mon.ActivePidMapLock)
+
+		ActivePidMapLock.Lock()
+
+		for _, pidMap := range ActivePidMap {
+			for pid, pidNode := range pidMap {
+				if pidNode.Exited {
+					if now.After(pidNode.ExitedTime.Add(time.Second * 5)) {
+						delete(pidMap, pid)
+					}
+				}
+			}
+		}
+
+		ActiveHostPidMap := *(mon.ActiveHostPidMap)
+
+		for _, pidMap := range ActiveHostPidMap {
+			for pid, pidNode := range pidMap {
+				if pidNode.Exited {
+					if now.After(pidNode.ExitedTime.Add(time.Second * 5)) {
+						delete(pidMap, pid)
+					}
+				}
+			}
+		}
+
+		ActivePidMapLock.Unlock()
+	}
+}
