71ed1993e44efd07fee54adbd457f90f80ad43dc
Jaehyun Nam <jaehyun.nam@outlook.com>
2021-08-16 04:26:14 +0000

    add license headers

--- pkg/KubeArmorAuditPolicy/api/v1/kubearmorauditpolicy_types.go
+++ pkg/KubeArmorAuditPolicy/api/v1/kubearmorauditpolicy_types.go
@@ -1,18 +1,5 @@
-/*
-Copyright 2021.
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
+// Copyright 2021 Authors of KubeArmor
+// SPDX-License-Identifier: Apache-2.0
 
 package v1
 
@@ -29,35 +16,35 @@ type EventType struct {
 	// +kubebuilder:validation:Pattern=^([0-9]+p[0-9]+s|[0-9]+p[0-9]+m)$
 	Rate string `json:"rate,omitempty"`
 
+	// file related arguments
+	// +kubebuilder:validation:Pattern=^\/([A-z0-9-_.]+\/)*([A-z0-9-_.]+)$
+	Path string `json:"path,omitempty"`
+	// 	+kubebuilder:validation:Pattern=^/([A-z0-9-.]+/)*([A-z0-9-.]+)+/$
+	Directory string `json:"dir,omitempty"`
+	// +kubebuilder:validation:Optional
+	Mode string `json:"mode,omitempty"`
+
 	// socket related arguments
 	// +kubebuilder:validation:Enum=ip;tcp;udp;raw;IP;TCP;UDP;RAW
 	Protocol string `json:"protocol,omitempty"`
 	// +kubebuilder:validation:Optional
-	Port string `json:"port,omitempty"`
-	// +kubebuilder:validation:Optional
 	Ipv4Addr string `json:"ipv4addr,omitempty"`
 	// +kubebuilder:validation:Optional
 	Ipv6Addr string `json:"ipv6addr,omitempty"`
-
-	// file related arguments
-	// +kubebuilder:validation:Pattern=^\/([A-z0-9-_.]+\/)*([A-z0-9-_.]+)$
-	Path string `json:"path,omitempty"`
 	// +kubebuilder:validation:Optional
-	Mode string `json:"mode,omitempty"`
-	// 	+kubebuilder:validation:Pattern=^/([A-z0-9-.]+/)*([A-z0-9-.]+)+/$
-	Directory string `json:"dir,omitempty"`
+	Port string `json:"port,omitempty"`
 }
 
 type AuditRuleType struct {
-	// +kubebuilder:validation:Default="*"
-	Process string `json:"process,omitempty"`
-
 	// +kubebuilder:validation:Optional
 	Severity SeverityType `json:"severity,omitempty"`
 	// +kubebuilder:validation:Optional
 	Tags []string `json:"tags,omitempty"`
 	// +kubebuilder:validation:Optional
 	Message string `json:"message,omitempty"`
+
+	// +kubebuilder:validation:Default="*"
+	Process string `json:"process,omitempty"`
 
 	Events []EventType `json:"events"`
 }
