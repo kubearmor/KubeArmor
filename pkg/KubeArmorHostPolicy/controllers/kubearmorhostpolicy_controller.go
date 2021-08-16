7e6a64514f7a15c986a47f3fbe47a3b96b9901e0
Jaehyun Nam <jaehyun.nam@outlook.com>
2021-08-16 04:27:52 +0000

    add license headers

--- pkg/KubeArmorHostPolicy/controllers/kubearmorhostpolicy_controller.go
+++ pkg/KubeArmorHostPolicy/controllers/kubearmorhostpolicy_controller.go
@@ -1,18 +1,5 @@
-/*
-Copyright 2020-2021 AccuKnox.
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
+// Copyright 2021 Authors of KubeArmor
+// SPDX-License-Identifier: Apache-2.0
 
 package controllers
 
@@ -78,7 +65,7 @@ func (r *KubeArmorHostPolicyReconciler) Reconcile(req ctrl.Request) (ctrl.Result
 
 POLICYERROR:
 	if policyErr != nil {
-		//Update PolicyStatus
+		// Update PolicyStatus
 		policy.Status.PolicyStatus = "Not OK"
 		err := r.Status().Update(ctx, policy)
 		log.Info("Invalid KubeArmorHostPolicy")
@@ -87,7 +74,7 @@ POLICYERROR:
 		return ctrl.Result{}, err
 	}
 
-	//Update PolicyStatus
+	// Update PolicyStatus
 	policy.Status.PolicyStatus = "OK"
 	_ = r.Status().Update(ctx, policy)
 	log.Info("Fetched KubeArmorHostPolicy")
