1476647740969027cea15f01f4996dad025d7da8
Ankur Kothiwal <ankur.kothiwal99@gmail.com>
2021-08-30 17:21:18 +0530

    update entrypoint handling
    Signed-off-by: Ankur Kothiwal <ankur.kothiwal@accuknox.com>

---
+++ KubeArmor/eventAuditor/BPF/entrypoint.bpf.h
@@ -0,0 +1,26 @@
+#include <linux/types.h>
+#ifdef asm_inline
+#undef asm_inline
+#define asm_inline asm
+#endif
+
+#include <linux/bpf.h>
+#include <bpf/bpf_helpers.h>
+
+typedef __u64 u64;
+
+#define BPF_MAP(_name, _type, _key_type, _value_type, _max_entries) \
+    struct bpf_map_def SEC("maps") _name = {                        \
+        .type = _type,                                              \
+        .key_size = sizeof(_key_type),                              \
+        .value_size = sizeof(_value_type),                          \
+        .max_entries = _max_entries,                                \
+    };
+
+#define BPF_HASH(_name, _key_type, _value_type) \
+    BPF_MAP(_name, BPF_MAP_TYPE_HASH, _key_type, _value_type, 10240);
+
+#define BPF_PERF_OUTPUT(_name) \
+    BPF_MAP(_name, BPF_MAP_TYPE_PERF_EVENT_ARRAY, int, __u32, 1024);
+
+char LICENSE[] SEC("license") = "GPL";
