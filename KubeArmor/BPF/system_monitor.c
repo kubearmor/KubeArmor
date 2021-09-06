kubearmor/KubeArmor

commit 082bf484b06d1dd0ef8fe87a931dae5a7ebacc1c

change MAX_BUF_ELEM_SIZE to 2048 Bytes

KubeArmor/BPF/system_monitor.c
@@ -34,7 +34,7 @@
 // == Structures == //
 
 #define MAX_BUFFER_SIZE   32768
-#define MAX_BUF_ELEM_SIZE 256
+#define MAX_BUF_ELEM_SIZE 2048
 #define MAX_STRING_SIZE   4096
 #define MAX_STR_ARR_ELEM  20
 

commit b83e06bdc30915759f017e424c77ed1ebfe9134b

change MAX_BUF_ELEM_SIZE to 512 bytes and remove drop condition
Fixes: 082bf48

Signed-off-by: Ayush Dwivedi <ayush.dwivedi@accuknox.com>

KubeArmor/BPF/system_monitor.c
@@ -32,7 +32,7 @@
 // == Structures == //
 
 #define MAX_BUFFER_SIZE   32768
-#define MAX_BUF_ELEM_SIZE 2048
+#define MAX_BUF_ELEM_SIZE 512
 #define MAX_STRING_SIZE   4096
 #define MAX_STR_ARR_ELEM  20
 
@@ -628,9 +628,6 @@ int syscall__execve(struct pt_regs *ctx,
 
     if (base_off >= (MAX_BUFFER_SIZE>>2)) {
         set_buffer_offset(0);
-        if (bpf_get_prandom_u32() % 2) {
-            return 0;
-        }
         base_off = 0;
         off = base_off;
         *off_global += MAX_BUF_ELEM_SIZE;
@@ -681,9 +678,6 @@ int trace_ret_execve(struct pt_regs *ctx)
 
     if (base_off >= (MAX_BUFFER_SIZE>>2)) {
         set_buffer_offset(0);
-        if (bpf_get_prandom_u32() % 2) {
-            return 0;
-        }
         base_off = 0;
         off = base_off;
         *off_global += MAX_BUF_ELEM_SIZE;
@@ -731,9 +725,6 @@ int syscall__execveat(struct pt_regs *ctx,
 
     if (base_off >= (MAX_BUFFER_SIZE>>2)) {
         set_buffer_offset(0);
-        if (bpf_get_prandom_u32() % 2) {
-            return 0;
-        }
         base_off = 0;
         off = base_off;
         *off_global += MAX_BUF_ELEM_SIZE;
@@ -786,9 +777,6 @@ int trace_ret_execveat(struct pt_regs *ctx)
 
     if (base_off >= (MAX_BUFFER_SIZE>>2)) {
         set_buffer_offset(0);
-        if (bpf_get_prandom_u32() % 2) {
-            return 0;
-        }
         base_off = 0;
         off = base_off;
         *off_global += MAX_BUF_ELEM_SIZE;
@@ -832,9 +820,6 @@ int trace_do_exit(struct pt_regs *ctx, long code)
 
     if (base_off >= (MAX_BUFFER_SIZE>>2)) {
         set_buffer_offset(0);
-        if (bpf_get_prandom_u32() % 2) {
-            return 0;
-        }
         base_off = 0;
         off = base_off;
         *off_global += MAX_BUF_ELEM_SIZE;
@@ -953,9 +938,6 @@ static __always_inline int trace_ret_generic(u32 id, struct pt_regs *ctx, u64 ty
 
     if (base_off >= (MAX_BUFFER_SIZE>>2)) {
         set_buffer_offset(0);
-        if (bpf_get_prandom_u32() % 2) {
-            return 0;
-        }
         base_off = 0;
         off = base_off;
         *off_global += MAX_BUF_ELEM_SIZE;