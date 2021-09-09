1fc9ffa06841614995f151b90108c947da25c131
daemon1024 <barun1024@gmail.com>
2021-09-09 14:38:23 +0530

    refactor CRD for client-gen

--- pkg/KubeArmorPolicy/main.go
+++ pkg/KubeArmorPolicy/main.go
@@ -14,7 +14,7 @@ import (
 	clientgoscheme "k8s.io/client-go/kubernetes/scheme"
 	_ "k8s.io/client-go/plugin/pkg/client/auth/gcp"
 
-	securityv1 "github.com/kubearmor/KubeArmor/pkg/KubeArmorPolicy/api/v1"
+	securityv1 "github.com/kubearmor/KubeArmor/pkg/KubeArmorPolicy/api/security.kubearmor.com/v1"
 	"github.com/kubearmor/KubeArmor/pkg/KubeArmorPolicy/controllers"
 	// +kubebuilder:scaffold:imports
 )
