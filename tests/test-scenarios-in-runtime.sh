c21052e11bd059224f16999896a35d15c74742f8
Jaehyun Nam <jaehyun.nam@outlook.com>
2021-10-07 12:53:07 +0000

    update test scripts

--- tests/test-scenarios-in-runtime.sh
+++ tests/test-scenarios-in-runtime.sh
@@ -142,11 +142,11 @@ function should_not_find_any_log() {
     KUBEARMOR=$(kubectl get pods -n kube-system -l kubearmor-app=kubearmor -o wide 2> /dev/null | grep $NODE | grep kubearmor | awk '{print $1}')
 
     if [[ $KUBEARMOR = "kubearmor"* ]]; then
-        audit_log=$(kubectl -n kube-system exec -it $KUBEARMOR -- tail -n 20 $ARMOR_LOG | grep -E "$1.*MatchedPolicy.*$2.*$3.*$4" | grep -v Passed)
+        audit_log=$(kubectl -n kube-system exec -it $KUBEARMOR -- grep -E "$1.*MatchedPolicy.*$2.*resource.*$3.*$4" $ARMOR_LOG | tail -n 1 | grep -v Passed)
         if [ $? == 0 ]; then
             if [ "$audit_log == $LAST_LOG" ]; then
                 audit_log="<No Log>"
-                DBG "Found no log from logs"
+                DBG "Found no log from logs (duplicated)"
             else
                 echo $audit_log
                 FAIL "Found the log from logs"
@@ -157,11 +157,11 @@ function should_not_find_any_log() {
             DBG "Found no log from logs"
         fi
     else # local
-        audit_log=$(tail -n 20 $ARMOR_LOG | grep -E "$1.*MatchedPolicy.*$2.*$3.*$4" | grep -v Passed)
+        audit_log=$(grep -E "$1.*MatchedPolicy.*$2.*resource.*$3.*$4" $ARMOR_LOG | tail -n 1 | grep -v Passed)
         if [ $? == 0 ]; then
             if [ "$audit_log" == "$LAST_LOG" ]; then
                 audit_log="<No Log>"
-                DBG "Found no log from logs"
+                DBG "Found no log from logs (duplicated)"
             else
                 echo $audit_log
                 FAIL "Found the log from logs"
@@ -185,7 +185,7 @@ function should_find_passed_log() {
     KUBEARMOR=$(kubectl get pods -n kube-system -l kubearmor-app=kubearmor -o wide 2> /dev/null | grep $NODE | grep kubearmor | awk '{print $1}')
 
     if [[ $KUBEARMOR = "kubearmor"* ]]; then
-        audit_log=$(kubectl -n kube-system exec -it $KUBEARMOR -- tail -n 20 $ARMOR_LOG | grep -E "$1.*MatchedPolicy.*$2.*$3.*$4" | grep Passed)
+        audit_log=$(kubectl -n kube-system exec -it $KUBEARMOR -- grep -E "$1.*MatchedPolicy.*$2.*resource.*$3.*$4" $ARMOR_LOG | tail -n 1 | grep Passed)
         if [ $? != 0 ]; then
             audit_log="<No Log>"
             FAIL "Failed to find the log from logs"
@@ -193,7 +193,7 @@ function should_find_passed_log() {
         else
             if [ "$audit_log" == "$LAST_LOG" ]; then
                 audit_log="<No Log>"
-                FAIL "Failed to find the log from logs"
+                FAIL "Failed to find the log from logs (duplicated)"
                 res_cmd=1
             else
                 echo $audit_log
@@ -201,7 +201,7 @@ function should_find_passed_log() {
             fi
         fi
     else # local
-        audit_log=$(tail -n 20 $ARMOR_LOG | grep -E "$1.*MatchedPolicy.*$2.*$3.*$4" | grep Passed)
+        audit_log=$(grep -E "$1.*MatchedPolicy.*$2.*resource.*$3.*$4" $ARMOR_LOG | tail -n 1 | grep Passed)
         if [ $? != 0 ]; then
             audit_log="<No Log>"
             FAIL "Failed to find the log from logs"
@@ -209,7 +209,7 @@ function should_find_passed_log() {
         else
             if [ "$audit_log" == "$LAST_LOG" ]; then
                 audit_log="<No Log>"
-                FAIL "Failed to find the log from logs"
+                FAIL "Failed to find the log from logs (duplicated)"
                 res_cmd=1
             else
                 echo $audit_log
@@ -235,7 +235,7 @@ function should_find_blocked_log() {
     fi
 
     if [[ $KUBEARMOR = "kubearmor"* ]]; then
-        audit_log=$(kubectl -n kube-system exec -it $KUBEARMOR -- tail -n 20 $ARMOR_LOG | grep -E "$1.*$match_type.*$2.*$3.*$4" | grep -v Passed)
+        audit_log=$(kubectl -n kube-system exec -it $KUBEARMOR -- grep -E "$1.*$match_type.*$2.*resource.*$3.*$4" $ARMOR_LOG | tail -n 1 | grep -v Passed)
         if [ $? != 0 ]; then
             audit_log="<No Log>"
             FAIL "Failed to find the log from logs"
@@ -243,7 +243,7 @@ function should_find_blocked_log() {
         else
             if [ "$audit_log" == "$LAST_LOG" ]; then
                 audit_log="<No Log>"
-                FAIL "Failed to find the log from logs"
+                FAIL "Failed to find the log from logs (duplicated)"
                 res_cmd=1
             else
                 echo $audit_log
@@ -251,7 +251,7 @@ function should_find_blocked_log() {
             fi
         fi
     else # local
-        audit_log=$(tail -n 20 $ARMOR_LOG | grep -E "$1.*$match_type.*$2.*$3.*$4" | grep -v Passed)
+        audit_log=$(grep -E "$1.*$match_type.*$2.*resource.*$3.*$4" $ARMOR_LOG | tail -n 1 | grep -v Passed)
         if [ $? != 0 ]; then
             audit_log="<No Log>"
             FAIL "Failed to find the log from logs"
@@ -259,7 +259,7 @@ function should_find_blocked_log() {
         else
             if [ "$audit_log" == "$LAST_LOG" ]; then
                 audit_log="<No Log>"
-                FAIL "Failed to find the log from logs"
+                FAIL "Failed to find the log from logs (duplicated)"
                 res_cmd=1
             else
                 echo $audit_log
@@ -280,11 +280,11 @@ function should_not_find_any_host_log() {
     KUBEARMOR=$(kubectl get pods -n kube-system -l kubearmor-app=kubearmor -o wide 2> /dev/null | grep $NODE | grep kubearmor | awk '{print $1}')
 
     if [[ $KUBEARMOR = "kubearmor"* ]]; then
-        audit_log=$(kubectl -n kube-system exec -it $KUBEARMOR -- tail -n 20 $ARMOR_LOG | grep -E "$HOST_NAME.*MatchedHostPolicy.*$1.*$2.*$3" | grep -v Passed)
+        audit_log=$(kubectl -n kube-system exec -it $KUBEARMOR -- grep -E "$HOST_NAME.*MatchedHostPolicy.*$1.*resource.*$2.*$3" $ARMOR_LOG | tail -n 1 | grep -v Passed)
         if [ $? == 0 ]; then
             if [ "$audit_log" == "$LAST_LOG" ]; then
                 audit_log="<No Log>"
-                DBG "Found no log from logs"
+                DBG "Found no log from logs (duplicated)"
             else
                 echo $audit_log
                 FAIL "Found the log from logs"
@@ -295,11 +295,11 @@ function should_not_find_any_host_log() {
             DBG "[INFO] Found no log from logs"
         fi
     else # local
-        audit_log=$(tail -n 20 $ARMOR_LOG | grep -E "$HOST_NAME.*MatchedHostPolicy.*$1.*$2.*$3" | grep -v Passed)
+        audit_log=$(grep -E "$HOST_NAME.*MatchedHostPolicy.*$1.*resource.*$2.*$3" $ARMOR_LOG | tail -n 1 | grep -v Passed)
         if [ $? == 0 ]; then
             if [ "$audit_log" == "$LAST_LOG" ]; then
                 audit_log="<No Log>"
-                DBG "Found no log from logs"
+                DBG "Found no log from logs (duplicated)"
             else
                 echo $audit_log
                 FAIL "Found the log from logs"
@@ -323,7 +323,7 @@ function should_find_passed_host_log() {
     KUBEARMOR=$(kubectl get pods -n kube-system -l kubearmor-app=kubearmor -o wide 2> /dev/null | grep $NODE | grep kubearmor | awk '{print $1}')
 
     if [[ $KUBEARMOR = "kubearmor"* ]]; then
-        audit_log=$(kubectl -n kube-system exec -it $KUBEARMOR -- tail -n 20 $ARMOR_LOG | grep -E "$HOST_NAME.*MatchedHostPolicy.*$1.*$2.*$3" | grep Passed)
+        audit_log=$(kubectl -n kube-system exec -it $KUBEARMOR -- grep -E "$HOST_NAME.*MatchedHostPolicy.*$1.*resource.*$2.*$3" $ARMOR_LOG | tail -n 1 | grep Passed)
         if [ $? != 0 ]; then
             audit_log="<No Log>"
             FAIL "Failed to find the log from logs"
@@ -331,7 +331,7 @@ function should_find_passed_host_log() {
         else
             if [ "$audit_log" == "$LAST_LOG" ]; then
                 audit_log="<No Log>"
-                FAIL "Failed to find the log from logs"
+                FAIL "Failed to find the log from logs (duplicated)"
                 res_cmd=1
             else
                 echo $audit_log
@@ -339,7 +339,7 @@ function should_find_passed_host_log() {
             fi
         fi    
     else # local
-        audit_log=$(tail -n 20 $ARMOR_LOG | grep -E "$HOST_NAME.*MatchedHostPolicy.*$1.*$2.*$3" | grep Passed)
+        audit_log=$(grep -E "$HOST_NAME.*MatchedHostPolicy.*$1.*resource.*$2.*$3" $ARMOR_LOG | tail -n 1 | grep Passed)
         if [ $? != 0 ]; then
             audit_log="<No Log>"
             FAIL "Failed to find the log from logs"
@@ -347,7 +347,7 @@ function should_find_passed_host_log() {
         else
             if [ "$audit_log" == "$LAST_LOG" ]; then
                 audit_log="<No Log>"
-                FAIL "Failed to find the log from logs"
+                FAIL "Failed to find the log from logs (duplicated)"
                 res_cmd=1
             else
                 echo $audit_log
@@ -373,7 +373,7 @@ function should_find_blocked_host_log() {
     fi
 
     if [[ $KUBEARMOR = "kubearmor"* ]]; then
-        audit_log=$(kubectl -n kube-system exec -it $KUBEARMOR -- tail -n 20 $ARMOR_LOG | grep -E "$HOST_NAME.*$match_type.*$1.*$2.*$3" | grep -v Passed)
+        audit_log=$(kubectl -n kube-system exec -it $KUBEARMOR -- grep -E "$HOST_NAME.*$match_type.*$1.*resource.*$2.*$3" $ARMOR_LOG | tail -n 1 | grep -v Passed)
         if [ $? != 0 ]; then
             audit_log="<No Log>"
             FAIL "Failed to find the log from logs"
@@ -381,7 +381,7 @@ function should_find_blocked_host_log() {
         else
             if [ "$audit_log" == "$LAST_LOG" ]; then
                 audit_log="<No Log>"
-                FAIL "Failed to find the log from logs"
+                FAIL "Failed to find the log from logs (duplicated)"
                 res_cmd=1
             else
                 echo $audit_log
@@ -389,7 +389,7 @@ function should_find_blocked_host_log() {
             fi
         fi
     else # local
-        audit_log=$(tail -n 20 $ARMOR_LOG | grep -E "$HOST_NAME.*$match_type.*$1.*$2.*$3" | grep -v Passed)
+        audit_log=$(grep -E "$HOST_NAME.*$match_type.*$1.*resource.*$2.*$3" $ARMOR_LOG | tail -n 1 | grep -v Passed)
         if [ $? != 0 ]; then
             audit_log="<No Log>"
             FAIL "Failed to find the log from logs"
@@ -397,7 +397,7 @@ function should_find_blocked_host_log() {
         else
             if [ "$audit_log" == "$LAST_LOG" ]; then
                 audit_log="<No Log>"
-                FAIL "Failed to find the log from logs"
+                FAIL "Failed to find the log from logs (duplicated)"
                 res_cmd=1
             else
                 echo $audit_log
