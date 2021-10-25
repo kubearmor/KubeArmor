a2ccd02601240526202dddb9b2d11b5ff75dd972
Elfadel Bonfoh <bonfohelfadel@gmail.com>
2021-10-25 13:53:54 +0200

    Fix ownerOnly policy match for Containerd

--- KubeArmor/core/containerdHandler.go
+++ KubeArmor/core/containerdHandler.go
@@ -155,6 +155,10 @@ func (ch *ContainerdHandler) GetContainerInfo(ctx context.Context, containerID s
 	spec := iface.(*specs.Spec)
 	container.AppArmorProfile = spec.Process.ApparmorProfile
 
+	preMergedDir := "/run/containerd/io.containerd.runtime.v2.task/k8s.io/"
+	postMergedDir := "/rootfs"
+	container.MergedDir = preMergedDir + res.Container.SnapshotKey + postMergedDir
+
 	// == //
 
 	taskReq := pt.ListPidsRequest{ContainerID: container.ContainerID}
