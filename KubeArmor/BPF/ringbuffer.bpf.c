ace917213af9c152267e2347ae4493fa8b91932b
Ankur Kothiwal <ankur.kothiwal99@gmail.com>
2021-11-01 12:09:39 +0530

    update ringbuffer implementation
    Signed-off-by: Ankur Kothiwal <ankur.kothiwal@accuknox.com>

--- KubeArmor/BPF/ringbuffer.bpf.c
+++ KubeArmor/BPF/ringbuffer.bpf.c
@@ -6,49 +6,51 @@
 #define asm_inline asm
 #endif
 
+#define TASK_COMM_LEN 16
+
 struct log_t{
-    u64 ts;
+    // u64 ts;
 
-    u32 pid_id;
-    u32 mnt_id;
+    // u32 pid_id;
+    // u32 mnt_id;
     
-    u32 host_ppid;
-    u32 host_pid;
+    // u32 host_ppid;
+    // u32 host_pid;
     
-    u32 ppid;
+    // u32 ppid;
     u32 pid;
-    u32 uid;
+    // u32 uid;
 
-    u32 event_id
+    // u32 event_id;
 
-};
+    //char comm[TASK_COMM_LEN];
+};  
 
 struct {
     __uint(type, BPF_MAP_TYPE_RINGBUF);
-    __uint(max_entries, 24);
-} ringbuff_map SEC(".maps");
+    __uint(max_entries, 1 << 24 );
+} ka_ea_ringbuff_map SEC(".maps");
 
 long flag = 0;
 
 SEC("tp/sched/sched_process_exec")
-int syscall__sys_execve(struct trace_event_raw_sched_process_exec *ctx) {
+int syscall__sys_execve(struct pt_regs *ctx) {
+    u64 cur_id = bpf_get_current_pid_tgid();
+
     struct log_t *log;
-    __u64 id = bpf_get_current_pid_tgid();
-    __u32 tgid = id >> 32;
-
-    log = bpf_map_lookup_elem(&ringbuff_map, 0);
-    if (!log) {
-        bpf_printk("Map not updated\n");
-        return 0;
-    }
-    log = bpf_ringbuf_reserve(&ringbuff_map, sizeof(int), BPF_RB_FORCE_WAKEUP);
     if (!log) {
         return 0;
     }
+   
+    log = bpf_ringbuf_reserve(&ka_ea_ringbuff_map, sizeof(*log), 0);
+    if(!log) {
+        return 0;
+    }
 
-    log->pid = tgid;
+    log->pid = cur_id >> 32;
+    //bpf_get_current_comm(log->comm, sizeof(log->comm));
 
-    bpf_ringbuf_submit(log, BPF_RB_FORCE_WAKEUP);
+    bpf_ringbuf_submit(log, flag);
     return 0;
 }
 
