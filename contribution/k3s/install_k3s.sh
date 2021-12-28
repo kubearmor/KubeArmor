e7775479e5bad288f4eac06a22ba5bf8c9e76290
Rahul Jadhav <nyrahul@gmail.com>
2021-12-28 21:31:54 +0530

    using docker as default
    Signed-off-by: Rahul Jadhav <nyrahul@gmail.com>

--- contribution/k3s/install_k3s.sh
+++ contribution/k3s/install_k3s.sh
@@ -3,7 +3,7 @@
 # Copyright 2021 Authors of KubeArmor
 
 # create a single-node K3s cluster
-curl -sfL https://get.k3s.io | K3S_KUBECONFIG_MODE="644" INSTALL_K3S_EXEC="--disable=traefik" sh -
+curl -sfL https://get.k3s.io | K3S_KUBECONFIG_MODE="644" INSTALL_K3S_EXEC="--disable=traefik --docker" sh -
 
 KUBEDIR=~/.kube
 KUBECONFIG=$KUBEDIR/config
