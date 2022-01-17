ee7ec8411b8653cab097d4f536014c53c8fdaa19
Jaehyun Nam <jn@accuknox.com>
2022-01-17 12:27:16 +0000

    update script
    Signed-off-by: Jaehyun Nam <jn@accuknox.com>

--- contribution/minikube/start_minikube.sh
+++ contribution/minikube/start_minikube.sh
@@ -2,11 +2,28 @@
 # SPDX-License-Identifier: Apache-2.0
 # Copyright 2021 Authors of KubeArmor
 
-# Refer to https://minikube.sigs.k8s.io/docs/tutorials/ebpf_tools_in_minikube
+realpath() {
+    CURR=$PWD
+
+    cd "$(dirname "$0")"
+    LINK=$(readlink "$(basename "$0")")
+
+    while [ "$LINK" ]; do
+        cd "$(dirname "$LINK")"
+        LINK=$(readlink "$(basename "$1")")
+    done
+
+    REALPATH="$PWD/$(basename "$1")"
+    echo "$REALPATH"
+
+    cd $CURR
+}
+
+MINIKUBE_DIR=`dirname $(realpath "$0")`
 
 # download minikube iso
-wget -O minikube.iso https://github.com/kubearmor/kastore/blob/main/iso/minikube/minikube.iso?raw=true
+wget -O $MINIKUBE_DIR/minikube.iso https://github.com/kubearmor/kastore/blob/main/iso/minikube/minikube.iso?raw=true
 
 # start minikube with a specific image
 
-minikube start --iso-url=file://$(pwd)/minikube.iso --cpus 4 --memory 4096 --cni flannel
+minikube start --iso-url=file://$MINIKUBE_DIR/minikube.iso --cpus 4 --memory 4096 --cni flannel
