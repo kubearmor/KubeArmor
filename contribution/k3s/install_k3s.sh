fc20f4467211f1e5012ac1564a7a75bad698743f
Jaehyun Nam <jn@accuknox.com>
2022-01-18 17:31:50 +0000

    update k3s
    Signed-off-by: Jaehyun Nam <jn@accuknox.com>

--- contribution/k3s/install_k3s.sh
+++ contribution/k3s/install_k3s.sh
@@ -3,12 +3,12 @@
 # Copyright 2021 Authors of KubeArmor
 
 # create a single-node K3s cluster
-curl -sfL https://get.k3s.io | K3S_KUBECONFIG_MODE="644" INSTALL_K3S_EXEC="--disable=traefik --docker" sh -
+curl -sfL https://get.k3s.io | K3S_KUBECONFIG_MODE="644" INSTALL_K3S_EXEC="--disable=traefik" sh -
 
-KUBEDIR=~/.kube
+KUBEDIR=$HOME/.kube
 KUBECONFIG=$KUBEDIR/config
 
-[[ ! -d $KUBEDIR ]] && mkdir ~/.kube/
+[[ ! -d $KUBEDIR ]] && mkdir $HOME/.kube/
 if [ -f $KUBECONFIG ]; then
 	KUBECONFIGBKP=$KUBEDIR/config.backup
 	echo "Found $KUBECONFIG already in place ... backing it up to $KUBECONFIGBKP"
