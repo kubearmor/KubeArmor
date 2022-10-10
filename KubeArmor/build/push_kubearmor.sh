dde2d8f598f085f48d94aaec2d47dc02b829ea6b
Anurag <contact.anurag7@gmail.com>
2022-10-11 01:41:57 +0530

    update ci workflow

--- KubeArmor/build/push_kubearmor.sh
+++ KubeArmor/build/push_kubearmor.sh
@@ -2,7 +2,7 @@
 # SPDX-License-Identifier: Apache-2.0
 # Copyright 2021 Authors of KubeArmor
 
-[[ "$REPO" == "" ]] && REPO="kubearmor/kubearmor"
+[[ "$REPO" == "" ]] && REPO="kranurag78/kubearmor"
 
 VERSION=latest
 
