7802e23a3d0ce659fe789424ac11718e94588731
PrimalPimmy <Prashant20.pm@gmail.com>
2023-02-07 20:55:53 +0530

    Adding tests for block posture
    Signed-off-by: PrimalPimmy <Prashant20.pm@gmail.com>

---
+++ tests/blockposture/blockposture_suite_test.go
@@ -0,0 +1,13 @@
+package blockposture_test
+
+import (
+	"testing"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+)
+
+func TestSmoke(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "Blockposture Suite")
+}
---
+++ tests/blockposture/posture_test.go
@@ -0,0 +1,93 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright 2022 Authors of KubeArmor
+
+package blockposture
+
+import (
+	"fmt"
+	"time"
+
+	. "github.com/kubearmor/KubeArmor/tests/util"
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+)
+
+var _ = BeforeSuite(func() {
+	// install wordpress-mysql app
+	err := K8sApply([]string{"res/wordpress-mysql-deployment.yaml"})
+	Expect(err).To(BeNil())
+
+	//annotate block posture on namespace level
+	_, err = Kubectl(fmt.Sprintf("annotate ns wordpress-mysql kubearmor-network-posture=block --overwrite"))
+	Expect(err).To(BeNil())
+
+	// delete all KSPs
+	err = DeleteAllKsp()
+	Expect(err).To(BeNil())
+
+	// enable kubearmor port forwarding
+	err = KubearmorPortForward()
+	Expect(err).To(BeNil())
+})
+
+var _ = AfterSuite(func() {
+	//remove block annotations after tests are done
+	_, err := Kubectl(fmt.Sprintf("annotate ns wordpress-mysql kubearmor-network-posture- --overwrite"))
+	Expect(err).To(BeNil())
+	KubearmorPortForwardStop()
+})
+
+func getWpsqlPod(name string, ant string) string {
+	pods, err := K8sGetPods(name, "wordpress-mysql", []string{ant}, 60)
+	Expect(err).To(BeNil())
+	Expect(len(pods)).To(Equal(1))
+	return pods[0]
+}
+
+var _ = Describe("Posture", func() {
+	var wp string
+	// var sql string
+
+	BeforeEach(func() {
+		wp = getWpsqlPod("wordpress-",
+			"container.apparmor.security.beta.kubernetes.io/wordpress: localhost/kubearmor-wordpress-mysql-wordpress-wordpress")
+		// sql = getWpsqlPod("mysql-",
+		// 	"container.apparmor.security.beta.kubernetes.io/mysql: localhost/kubearmor-wordpress-mysql-mysql-mysql")
+	})
+
+	AfterEach(func() {
+		KarmorLogStop()
+		err := DeleteAllKsp()
+		Expect(err).To(BeNil())
+		// wait for policy deletion
+		time.Sleep(5 * time.Second)
+	})
+
+	Describe("Policy Apply", func() {
+		It("can whitelist use of certain network protocols used by a package, such as tcp", func() {
+			// Apply policy
+			err := K8sApplyFile("res/ksp-wordpress-allow-tcp.yaml")
+			Expect(err).To(BeNil())
+
+			// Start Kubearmor Logs
+			err = KarmorLogStart("policy", "wordpress-mysql", "Network", wp)
+			Expect(err).To(BeNil())
+
+			// wait for policy creation
+			time.Sleep(5 * time.Second)
+
+			sout, _, err := K8sExecInPod(wp, "wordpress-mysql", []string{"bash", "-c", "curl google.com"})
+			Expect(err).To(BeNil())
+			fmt.Printf("---START---\n%s---END---\n", sout)
+			Expect(sout).To(MatchRegexp("curl.*Could not resolve host: google.com"))
+
+			// check policy violation alert
+			_, alerts, err := KarmorGetLogs(5*time.Second, 1)
+			Expect(err).To(BeNil())
+			Expect(len(alerts)).To(BeNumerically(">=", 1))
+			Expect(alerts[0].PolicyName).To(Equal("DefaultPosture"))
+			Expect(alerts[0].Action).To(Equal("Block"))
+		})
+	})
+
+})
