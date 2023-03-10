574bf7bfda78d5ea594bd3e0ad0860834803be50
Ononiwu Maureen <amaka013@gmail.com>
2023-03-10 11:54:50 +0100

    Set default GRPC listening port for kubearmor as environment variable in post-install script
    Signed-off-by: Ononiwu Maureen <amaka013@gmail.com>

--- KubeArmor/packaging/post-install.sh
+++ KubeArmor/packaging/post-install.sh
@@ -19,3 +19,7 @@ fi
 # start kubearmor.service
 /bin/systemctl daemon-reload
 /bin/systemctl start kubearmor.service
+
+# Set default GRPC listening port for kubearmor as environment variable
+echo "export KUBEARMOR_SERVICE=:32767" >> ~/.bashrc
+source ~/.bashrc
