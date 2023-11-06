86ff822486cbc383b63b7c95b86262ea6b49e583
Ankur Kothiwal <ankur.kothiwal99@gmail.com>
2023-11-06 16:50:33 +0530

    fix killercoda kubearmor-installation script
    Signed-off-by: Ankur Kothiwal <ankur.kothiwal99@gmail.com>

--- contribution/Killercoda-Kubearmor/kubearmor-installation-view.sh
+++ contribution/Killercoda-Kubearmor/kubearmor-installation-view.sh
@@ -3,15 +3,12 @@
 # Copyright 2023 Authors of KubeArmor
 
 namespace="kubearmor"
-
-echo "Waiting for all pods in namespace '$namespace' to be in the 'Running' state"
-
-kubectl wait --for=condition=ready --timeout=5m -n kubearmor pod -l kubearmor-app=kubearmor-operator
-kubectl get po -n $namespace
-kubectl wait -n kubearmor --timeout=5m --for=jsonpath='{.status.phase}'=Running kubearmorconfigs/kubearmor-default
-kubectl wait --timeout=5m --for=condition=ready pod -l kubearmor-app,kubearmor-app!=kubearmor-snitch -n kubearmor
-kubectl wait --timeout=5m --for=condition=ready pod -l kubearmor-app=kubearmor,kubearmor-app!=kubearmor-snitch -n kubearmor
-
-echo "All pods in namespace '$namespace' are now in the 'Running' state"
-
-kubectl get po -n $namespace
+duration=120
+watch -n 1 "kubectl get po -n $namespace" &
+watch_pid=$!
+sleep $duration
+while kubectl get po -n $namespace | tail -n +2 | awk '{print $3}' | grep -q -v "Running"; do
+  sleep 8
+done
+kill $watch_pid
+echo "All pods are in the 'Running' state. Exiting..."
