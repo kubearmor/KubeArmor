5ec11b868e7963bbbe3c2638e3133e4255429185
PrimalPimmy <Prashant20.pm@gmail.com>
2024-03-14 14:42:03 +0530

    fix(operator): Seccomp defaulted to false and ARM based seccomp added
    Signed-off-by: PrimalPimmy <Prashant20.pm@gmail.com>
    
    fix(operator): arm syscall for init
    
    Signed-off-by: PrimalPimmy <Prashant20.pm@gmail.com>
    
    fix(operator): arm syscall for init-2
    
    Signed-off-by: PrimalPimmy <Prashant20.pm@gmail.com>

--- pkg/KubeArmorOperator/seccomp/seccomp.go
+++ pkg/KubeArmorOperator/seccomp/seccomp.go
@@ -18,7 +18,7 @@ func LoadSeccompInNode() {
 
 	seccompProfile := Seccomp{
 		DefaultAction: ActErrno,
-		Architectures: []string{Archx32, Archx86_64, Archx86},
+		Architectures: []string{Archx32, Archx86_64, Archx86, ArchARM, ArchARM64},
 		Syscalls: []*Syscalls{{
 			Names: []string{
 				"getsockopt",
@@ -126,7 +126,7 @@ func LoadSeccompInNode() {
 
 	seccompInitProfile := Seccomp{
 		DefaultAction: ActErrno,
-		Architectures: []string{Archx32, Archx86_64, Archx86},
+		Architectures: []string{Archx32, Archx86_64, Archx86, ArchARM, ArchARM64},
 		Syscalls: []*Syscalls{{
 			Names: []string{
 				"dup2",
@@ -138,6 +138,8 @@ func LoadSeccompInNode() {
 				"memfd_create",
 				"capset",
 				"read",
+				"dup3",
+				"getpgid",
 				"getrandom",
 				"close",
 				"fchown",
@@ -166,6 +168,7 @@ func LoadSeccompInNode() {
 				"capget",
 				"sysinfo",
 				"connect",
+				"pipe2",
 				"openat",
 				"access",
 				"set_robust_list",
