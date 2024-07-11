8f8669bb97f88be62e181665f922e58fbff3a367
Anurag Rajawat <anuragsinghrajawat22@gmail.com>
2024-07-11 20:16:51 +0530

    fix: Update relay clusterrole to watch pods
    Signed-off-by: Anurag Rajawat <anuragsinghrajawat22@gmail.com>

--- deployments/get/objects.go
+++ deployments/get/objects.go
@@ -6,7 +6,6 @@ package deployments
 import (
 	"strconv"
 
-	cfg "github.com/kubearmor/KubeArmor/KubeArmor/config"
 	admissionregistrationv1 "k8s.io/api/admissionregistration/v1"
 	appsv1 "k8s.io/api/apps/v1"
 	corev1 "k8s.io/api/core/v1"
@@ -14,6 +13,8 @@ import (
 	"k8s.io/apimachinery/pkg/api/resource"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/util/intstr"
+
+	cfg "github.com/kubearmor/KubeArmor/KubeArmor/config"
 )
 
 // GetServiceAccount Function
@@ -220,7 +221,7 @@ func GetRelayClusterRole() *rbacv1.ClusterRole {
 			{
 				APIGroups: []string{""},
 				Resources: []string{"pods"},
-				Verbs:     []string{"get", "list"},
+				Verbs:     []string{"list", "watch"},
 			},
 		},
 	}
