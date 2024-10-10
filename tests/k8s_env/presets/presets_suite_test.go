29ec5c399199a9fa9709b5a3c628f22a95a890ae
rksharma95 <ramakant@accuknox.com>
2024-10-10 23:43:18 +0530

    add e2e tests for filelessexecution preset
    Signed-off-by: rksharma95 <ramakant@accuknox.com>

---
+++ tests/k8s_env/presets/presets_suite_test.go
@@ -0,0 +1,13 @@
+package presets_test
+
+import (
+	"testing"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+)
+
+func TestPresets(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "Presets Suite")
+}
