9b861b05bcf5f4005b06b169e2867715645b3dfe
Devisri <devisris.it2022@citchennai.net>
2025-03-26 18:07:03 +0530

    Updated crio installation to support ubuntu version 22.04
    Signed-off-by: Devisri S <devisris.it2022@citchennai.net>

--- contribution/self-managed-k8s/crio/install_crio.sh
+++ contribution/self-managed-k8s/crio/install_crio.sh
@@ -4,16 +4,20 @@
 
 . /etc/os-release
 
-if [ "$NAME" != "Ubuntu" ]; then
-    echo "Support Ubuntu 18.xx, 20.xx"
-    exit
+# Check for supported Ubuntu versions
+if [[ "$NAME" != "Ubuntu" || ! "$VERSION_ID" =~ 20.04|22.04 ]]; then
+    echo "Unsupported OS version. This script supports Ubuntu 20.04 and 22.04."
+    exit 1
 fi
+
 
 OS="x${NAME}_${VERSION_ID}"
 
 # install cri-o corresponding to the latest k3s version
+
 VERSION=$(curl -w '%{url_effective}' -L -s -S https://update.k3s.io/v1-release/channels/stable -o /dev/null | sed -e 's|.*/||' | cut -d '.' -f1,2)
 echo "Installing CRI-O version $VERSION"
+
 
 curl -fsSL https://pkgs.k8s.io/addons:/cri-o://stable:/$VERSION/deb/Release.key |
     sudo gpg --dearmor -o /etc/apt/keyrings/cri-o-apt-keyring.gpg
@@ -26,10 +30,14 @@ sudo apt-get update
 sudo apt-get install -y cri-o
 sudo dpkg -i --force-overwrite /var/cache/apt/archives/cri-o_*.deb
 
-# this option is not supported in ubuntu 18.04
-if [ "$VERSION_ID" == "18.04" ]; then
-    sudo sed -i 's/,metacopy=on//g' /etc/containers/storage.conf
-fi
 
 sudo systemctl daemon-reload
 sudo systemctl start crio.service
+
+# Verify installation
+if systemctl is-active --quiet crio.service; then
+    echo "CRI-O installation successful and service is running."
+else
+    echo "CRI-O installation failed or service did not start. Check logs using 'journalctl -u crio.service'."
+    exit 1
+fi
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Devisrisamidurai/KubeArmor
+
+go 1.23.6
