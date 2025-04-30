e57524c5a2728c2ceff3b0177bd599ad66456a7b
Arnav Sharma <arnavsharma0703@gmail.com>
2025-04-30 04:48:21 -0400

    Refactor visibility log generation and add unittest
    Signed-off-by: Arnav Sharma <arnavsharma0703@gmail.com>

--- KubeArmor/feeder/feeder_test.go
+++ KubeArmor/feeder/feeder_test.go
@@ -4,11 +4,13 @@
 package feeder
 
 import (
+	"reflect"
 	"sync"
 	"testing"
 
 	cfg "github.com/kubearmor/KubeArmor/KubeArmor/config"
 	tp "github.com/kubearmor/KubeArmor/KubeArmor/types"
+	pb "github.com/kubearmor/KubeArmor/protobuf"
 )
 
 func TestFeeder(t *testing.T) {
@@ -36,4 +38,66 @@ func TestFeeder(t *testing.T) {
 		return
 	}
 	t.Log("[PASS] Destroyed logger")
+}
+
+func TestMarshalVisibilityLog(t *testing.T) {
+	// example visibility log - hostname field excluded since it is set
+	// in the feeder and this test just checks marshaling
+	visibilityLog := tp.Log{
+		ClusterName:       "default",
+		Type:              "HostLog",
+		Source:            "/usr/bin/dockerd",
+		Resource:          "/usr/bin/runc --version",
+		Operation:         "Process",
+		Data:              "syscall=SYS_EXECVE",
+		Result:            "Passed",
+		HostPID:           193088,
+		HostPPID:          914,
+		PID:               193088,
+		PPID:              914,
+		ParentProcessName: "/usr/bin/dockerd",
+		ProcessName:       "/usr/bin/runc",
+	}
+
+	expectedMarshaledLog := &pb.Log{
+		ClusterName:       "default",
+		Type:              "HostLog",
+		Source:            "/usr/bin/dockerd",
+		Resource:          "/usr/bin/runc --version",
+		Operation:         "Process",
+		Data:              "syscall=SYS_EXECVE",
+		Result:            "Passed",
+		HostPID:           193088,
+		HostPPID:          914,
+		PID:               193088,
+		PPID:              914,
+		ParentProcessName: "/usr/bin/dockerd",
+		ProcessName:       "/usr/bin/runc",
+	}
+
+	t.Run("WithResource", func(t *testing.T) {
+		originalDropResource := cfg.GlobalCfg.DropResourceFromProcessLogs
+		defer func() { cfg.GlobalCfg.DropResourceFromProcessLogs = originalDropResource }()
+		cfg.GlobalCfg.DropResourceFromProcessLogs = false
+
+		marshaledLog := MarshalVisibilityLog(visibilityLog)
+		if !reflect.DeepEqual(marshaledLog, expectedMarshaledLog) {
+			t.Errorf("[FAIL] Expected marshaled log: %+v\nGot: %+v", expectedMarshaledLog, marshaledLog)
+		}
+	})
+
+	t.Run("WithoutResource", func(t *testing.T) {
+		originalDropResource := cfg.GlobalCfg.DropResourceFromProcessLogs
+		defer func() { cfg.GlobalCfg.DropResourceFromProcessLogs = originalDropResource }()
+		cfg.GlobalCfg.DropResourceFromProcessLogs = true
+
+		expectedWithoutResource := &pb.Log{}
+		*expectedWithoutResource = *expectedMarshaledLog
+		expectedWithoutResource.Resource = ""
+
+		marshaledLog := MarshalVisibilityLog(visibilityLog)
+		if !reflect.DeepEqual(marshaledLog, expectedWithoutResource) {
+			t.Errorf("[FAIL] Expected marshaled log: %+v\nGot: %+v", expectedWithoutResource, marshaledLog)
+		}
+	})
 }
