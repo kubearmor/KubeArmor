b563faec810de52ce7d262f9f846cf6c706e081f
Aryan Bakliwal <aryanbakliwal12345@gmail.com>
2026-01-02 14:38:25 +0530

    increase log and alert timeout in CI tests
    Signed-off-by: Aryan Bakliwal <aryanbakliwal12345@gmail.com>

--- tests/k8s_env/blockposture/block_test.go
+++ tests/k8s_env/blockposture/block_test.go
@@ -13,14 +13,14 @@ import (
 )
 
 var _ = BeforeSuite(func() {
-	// install wordpress-mysql app
-	err := K8sApply([]string{"res/wordpress-mysql-deployment.yaml"})
+	// deploy nginx app
+	err := K8sApply([]string{"res/nginx-posture-deployment.yaml"})
 	Expect(err).To(BeNil())
 	//annotate block posture on namespace level
-	_, err = Kubectl("annotate ns wordpress-mysql kubearmor-network-posture=block --overwrite")
+	_, err = Kubectl("annotate ns nginx-posture kubearmor-network-posture=block --overwrite")
 	Expect(err).To(BeNil())
 
-	_, err = Kubectl("annotate ns wordpress-mysql kubearmor-file-posture=block --overwrite")
+	_, err = Kubectl("annotate ns nginx-posture kubearmor-file-posture=block --overwrite")
 	Expect(err).To(BeNil())
 
 	//give some time for the deployment to be up before applying whitelisting policies
@@ -31,25 +31,24 @@ var _ = BeforeSuite(func() {
 })
 
 var _ = AfterSuite(func() {
-	// delete wordpress-mysql app
-	err := K8sDelete([]string{"res/wordpress-mysql-deployment.yaml"})
+	// delete nginx app
+	err := K8sDelete([]string{"res/nginx-posture-deployment.yaml"})
 	Expect(err).To(BeNil())
 })
 
-func getWpsqlPod(name string, ant string) string {
-	pods, err := K8sGetPods(name, "wordpress-mysql", []string{ant}, 60)
+func getNginxPod(name string, ant string) string {
+	pods, err := K8sGetPods(name, "nginx-posture", []string{ant}, 60)
 	Expect(err).To(BeNil())
 	Expect(len(pods)).To(Equal(1))
 	return pods[0]
 }
 
 var _ = Describe("Posture", func() {
-	var wp string
-	// var sql string
+
+	var ng string
 
 	BeforeEach(func() {
-		wp = getWpsqlPod("wordpress-", "kubearmor-policy: enabled")
-
+		ng = getNginxPod("nginx-posture-", "kubearmor-policy: enabled")
 	})
 
 	AfterEach(func() {
@@ -62,32 +61,30 @@ var _ = Describe("Posture", func() {
 
 	Describe("Policy Apply", func() {
 		It("can whitelist certain files accessed by a package while blocking all other sensitive content", func() {
-			err := util.AnnotateNS("wordpress-mysql", "kubearmor-file-posture", "block")
+			err := util.AnnotateNS("nginx-posture", "kubearmor-file-posture", "block")
 			Expect(err).To(BeNil())
 			// Apply policy
-			err = K8sApplyFile("res/ksp-wordpress-allow-file.yaml")
+			err = K8sApplyFile("res/ksp-nginx-allow-file.yaml")
 			Expect(err).To(BeNil())
 
 			// wait for policy creation, added due to flaky behaviour
-			time.Sleep(25 * time.Second)
+			time.Sleep(10 * time.Second)
 
 			// Start Kubearmor Logs
-			err = KarmorLogStart("policy", "wordpress-mysql", "File", wp)
+			err = KarmorLogStart("policy", "nginx-posture", "File", ng)
 			Expect(err).To(BeNil())
 
-			//curl needs UDP for DNS resolution
 			AssertCommand(
-				wp, "wordpress-mysql", []string{"bash", "-c", "cat wp-config.php"},
+				ng, "nginx-posture", []string{"sh", "-c", "cat docker-entrypoint.sh"},
 				MatchRegexp("cat.*Permission denied"), true,
 			)
 
-			//test that tcp is whitelisted
 			AssertCommand(
-				wp, "wordpress-mysql", []string{"bash", "-c", "cat readme.html"},
+				ng, "nginx-posture", []string{"sh", "-c", "cat /usr/share/nginx/html/index.html"},
 				MatchRegexp("<!DOCTYPE html>((?:.*\r?\n?)*)</html>"), true,
 			)
 			// check policy violation alert
-			_, alerts, err := KarmorGetLogs(5*time.Second, 1)
+			_, alerts, err := KarmorGetLogs(10*time.Second, 1)
 			Expect(err).To(BeNil())
 			Expect(len(alerts)).To(BeNumerically(">=", 1))
 			Expect(alerts[0].PolicyName).To(Equal("DefaultPosture"))
