6528c9f79f1dd7844f0794d55b615f5003a3c738
WHOIM1205 <rathourprateek8@gmail.com>
2026-01-17 21:55:52 -0800

    Fix goroutine leak in TraceEvents on daemon shutdown
    Signed-off-by: WHOIM1205 <rathourprateek8@gmail.com>

--- KubeArmor/enforcer/bpflsm/enforcer.go
+++ KubeArmor/enforcer/bpflsm/enforcer.go
@@ -321,8 +321,11 @@ func (be *BPFEnforcer) TraceEvents() {
 	}()
 
 	for {
-
-		dataRaw := <-be.EventsChannel
+		dataRaw, ok := <-be.EventsChannel
+		if !ok {
+			be.Logger.Print("EventsChannel closed, exiting TraceEvents")
+			return
+		}
 
 		var event eventBPF
 
@@ -510,6 +513,10 @@ func (be *BPFEnforcer) DestroyBPFEnforcer() error {
 			be.Logger.Err(err.Error())
 			errBPFCleanUp = errors.Join(errBPFCleanUp, err)
 		}
+	}
+
+	if be.EventsChannel != nil {
+		close(be.EventsChannel)
 	}
 
 	be = nil
