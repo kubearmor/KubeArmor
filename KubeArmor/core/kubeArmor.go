3695e11181efa7e36047adb86c12bf303d0533a3
Aryan Bakliwal <aryanbakliwal12345@gmail.com>
2026-03-06 09:11:26 +0530

    feat(operator): improve CRI socket detection
    Signed-off-by: Aryan Bakliwal <aryanbakliwal12345@gmail.com>

--- KubeArmor/core/kubeArmor.go
+++ KubeArmor/core/kubeArmor.go
@@ -778,6 +778,14 @@ func KubeArmor() {
 		}
 	}
 
+	var runtimeLabel, socketLabel string
+	if v, ok := dm.Node.Labels["kubearmor.io/runtime"]; ok {
+		runtimeLabel = v
+	}
+	if v, ok := dm.Node.Labels["kubearmor.io/socket"]; ok {
+		socketLabel = v
+	}
+
 	if dm.K8sEnabled && cfg.GlobalCfg.Policy {
 		if cfg.GlobalCfg.UseOCIHooks &&
 			(strings.Contains(dm.Node.ContainerRuntimeVersion, "cri-o") ||
@@ -816,6 +824,63 @@ func KubeArmor() {
 			}
 
 			dm.Logger.Printf("Using %s for monitoring containers", cfg.GlobalCfg.CRISocket)
+
+		} else if runtimeLabel != "" && socketLabel != "" { // use runtime and CRI socket detected by snitch
+
+			dm.Logger.Print("Using runtime and CRI socket detected by snitch")
+
+			// Whatever the socket file path detected by snitch is, when mounting it to daemonset the operator standardizes it with a default path
+			var podMountPaths = map[string]string{
+				"docker":     "/var/run/docker.sock",
+				"containerd": "/var/run/containerd/containerd.sock",
+				"cri-o":      "/var/run/crio/crio.sock",
+			}
+			socketFilePath, ok := podMountPaths[runtimeLabel]
+			if ok {
+				cfg.GlobalCfg.CRISocket = "unix://" + socketFilePath
+			}
+
+			switch runtimeLabel {
+			case "docker":
+				if _, err := os.Stat(socketFilePath); err == nil {
+					// update already deployed containers
+					dm.GetAlreadyDeployedDockerContainers()
+
+					// monitor docker events
+					go dm.MonitorDockerEvents()
+				} else {
+					dm.Logger.Err("Failed to monitor containers (Docker socket file is not accessible)")
+
+					// destroy the daemon
+					dm.DestroyKubeArmorDaemon()
+
+					return
+				}
+			case "containerd":
+				if _, err := os.Stat(socketFilePath); err == nil {
+					// monitor containerd events
+					go dm.MonitorContainerdEvents()
+				} else {
+					dm.Logger.Err("Failed to monitor containers (Containerd socket file is not accessible)")
+
+					// destroy the daemon
+					dm.DestroyKubeArmorDaemon()
+
+					return
+				}
+			case "cri-o":
+				if _, err := os.Stat(socketFilePath); err == nil {
+					// monitor cri-o events
+					go dm.MonitorCrioEvents()
+				} else {
+					dm.Logger.Err("Failed to monitor containers (CRI-O socket file is not accessible)")
+
+					// destroy the daemon
+					dm.DestroyKubeArmorDaemon()
+
+					return
+				}
+			}
 
 		} else { // CRI socket not set, we'll have to auto detect
 			dm.Logger.Print("CRI socket not set. Trying to detect.")
