f21d476acbc64bfd8d37889c91fbf5c0574b4613
Aryan Bakliwal <aryanbakliwal12345@gmail.com>
2026-07-29 10:14:07 +0530

    feat(ci): LVH kernel compatibility test matrix
    Signed-off-by: Aryan Bakliwal <aryanbakliwal12345@gmail.com>

---
+++ KubeArmor/enforcer/bpflsm/ci_loader/main.go
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright 2026 Authors of KubeArmor
+
+package main
+
+import "github.com/kubearmor/KubeArmor/KubeArmor/enforcer/bpflsm"
+
+func main() {
+	bpflsm.TestCILoad()
+}
