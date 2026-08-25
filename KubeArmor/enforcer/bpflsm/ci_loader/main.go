cab853e68f910bbb8cce108d5ca3f08d3821adc8
Aryan Bakliwal <aryanbakliwal12345@gmail.com>
2026-08-25 16:33:50 +0530

    fix go lint warning
    Signed-off-by: Aryan Bakliwal <aryanbakliwal12345@gmail.com>

--- KubeArmor/enforcer/bpflsm/ci_loader/main.go
+++ KubeArmor/enforcer/bpflsm/ci_loader/main.go
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: Apache-2.0
 // Copyright 2026 Authors of KubeArmor
 
+// Package main runs the BPF-LSM bytecode load test.
 package main
 
 import "github.com/kubearmor/KubeArmor/KubeArmor/enforcer/bpflsm"
